dcac02bb3dcab4b79ce4385feaac71029a402b18
oleavr <oleavr@60f4c359-591d-0410-bfb7-312270ef6092>
2006-09-13 18:33:21 +0000

    Initial checkin.

---
+++ Agent/hooking.h
@@ -0,0 +1,213 @@
+/**
+ * Copyright (C) 2006  Ole André Vadla Ravnås <oleavr@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#pragma once
+
+/*
+ * WARNING: This code in particular isn't 64-bit compliant, but might
+ *          be in the future if I get around to installing a 64-bit
+ *          Windows OS (which is highly unlikely considering that I
+ *          only use it for REing).  Contributions are very welcome. :-)
+ */
+
+#include "util.h"
+
+/****************************************************************************
+ * Useful hooking macros taking care of all the dirty work                  *
+ ****************************************************************************/
+
+#define HOOK_GLUE(name, args_size) \
+  static LPVOID name##_start = NULL; \
+  \
+  __declspec(naked) static void name##_done_proxy() \
+  { \
+    __asm { \
+      /* Store return value (eax) between return address and first argument */ \
+      __asm sub esp, 4 \
+      __asm push ebp \
+      __asm mov ebp, [esp+8] \
+      __asm mov [esp+4], ebp \
+      __asm mov [esp+8], eax \
+      __asm pop ebp \
+      \
+      /* Call our C callback */ \
+      __asm jmp name##_done \
+    } \
+  } \
+  \
+  __declspec(naked) static void name##_hook() \
+  { \
+    __asm { \
+      /* Calculate size of retaddr + args */ \
+      __asm mov edx, 4            /* edx contains the total size of */ \
+      __asm add edx, args_size    /* return address + all arguments. */ \
+      \
+      __asm mov ecx, edx          /* ecx contains edx divided by 4 */ \
+      __asm shr ecx, 2            /* to speed up the copying. */ \
+      \
+      /* Make a copy of retaddr + args onto the top of the stack */ \
+      __asm sub esp, edx \
+      __asm push esi \
+      __asm push edi \
+      __asm cld \
+      __asm lea edi, [8 + esp] \
+      __asm mov esi, edi \
+      __asm add esi, edx \
+      __asm rep movsd \
+      __asm pop edi \
+      __asm pop esi \
+      \
+      /* Call our C callback */ \
+      __asm call name##_called \
+      \
+      /* Overwrite return address so that we can trap the return. */ \
+      __asm mov ecx, name##_done_proxy \
+      __asm mov dword ptr [esp], ecx \
+      \
+      /* Set up stack frame (we need to do this because we've */ \
+      /* overwritten this code in the start of the original function) */ \
+      __asm push ebp \
+      __asm mov ebp, esp \
+      \
+      /* Continue */ \
+      __asm mov ecx, [name##_start] \
+      __asm add ecx, 5 \
+      __asm jmp ecx \
+    } \
+  }
+
+#define HOOK_GLUE_INTERRUPTIBLE(name, args_size) \
+  static LPVOID name##_start = NULL; \
+  \
+  __declspec(naked) static void name##_done_proxy() \
+  { \
+    __asm { \
+      /* Store return value (eax) between return address and first argument */ \
+      __asm sub esp, 4 \
+      __asm push ebp \
+      __asm mov ebp, [esp+8] \
+      __asm mov [esp+4], ebp \
+      __asm mov [esp+8], eax \
+      __asm pop ebp \
+      \
+      /* Call our C callback */ \
+      __asm jmp name##_done \
+    } \
+  } \
+  \
+  __declspec(naked) static void name##_hook() \
+  { \
+    __asm { \
+      /* Calculate size of retaddr + args */ \
+      __asm mov edx, 4 /* edx contains the total size of */ \
+      __asm add edx, args_size    /* return address + all arguments. */ \
+      \
+      __asm mov ecx, edx          /* ecx contains edx divided by 4 */ \
+      __asm shr ecx, 2            /* to speed up the copying. */ \
+      \
+      /* Make a copy of retaddr + args onto the top of the stack */ \
+      __asm sub esp, edx \
+      __asm push esi \
+      __asm push edi \
+      __asm cld \
+      __asm lea edi, [8 + esp] \
+      __asm mov esi, edi \
+      __asm add esi, edx \
+      __asm rep movsd \
+      __asm pop edi \
+      __asm pop esi \
+      \
+      /* Push an extra argument through which the callback can signal \
+       * that it doesn't want to carry on but return to caller. \
+       */ \
+      __asm push TRUE \
+      \
+      /* Call our C callback */ \
+      __asm call name##_called \
+      \
+      __asm pop edx \
+      __asm cmp edx, FALSE \
+      __asm jnz CARRY_ON /* should we carry on or return to caller? */ \
+      \
+      /* Return to caller */ \
+      __asm mov ecx, [esp] /* store return address */ \
+      __asm mov edx, args_size \
+      __asm add edx, 4 \
+      __asm shl edx, 1 \
+      __asm add esp, edx /* clear off both copies of (retaddr + args) */ \
+      __asm jmp ecx      /* return */ \
+      \
+      __asm CARRY_ON: \
+      /* Overwrite return address so that we can trap the return. */ \
+      __asm mov ecx, name##_done_proxy \
+      __asm mov dword ptr [esp], ecx \
+      \
+      /* Set up stack frame (we need to do this because we've */ \
+      /* overwritten this code in the start of the original function) */ \
+      __asm push ebp \
+      __asm mov ebp, esp \
+      \
+      /* Continue */ \
+      __asm mov ecx, [name##_start] \
+      __asm add ecx, 5 \
+      __asm jmp ecx \
+    } \
+  }
+
+#define HOOK_FUNCTION(handle, name) HOOK_FUNCTION_BY_ALIAS(handle, name, name)
+
+#define HOOK_FUNCTION_BY_ALIAS(handle, name, alias) \
+  { \
+    unsigned char signature[5] = { \
+      0x8B, 0xFF, /* mov edi, edi */ \
+      0x55,       /* push ebp */ \
+      0x8B, 0xEC  /* mov ebp, esp */ \
+    }; \
+    \
+    name##_start = GetProcAddress(handle, #name); \
+    if (name##_start != NULL) \
+    { \
+      if (address_has_bytes(name##_start, signature, sizeof(signature))) \
+      { \
+        write_jmp_instruction_to_addr(name##_start, name##_hook); \
+      } \
+      else \
+      { \
+        MessageBox(0, "Signature of " #name " is incompatible", \
+                      "oSpy", MB_ICONWARNING | MB_OK); \
+      } \
+    } \
+    else \
+    { \
+      MessageBox(0, "GetProcAddress of " #name " failed", \
+                    "oSpy", MB_ICONWARNING | MB_OK); \
+    } \
+  }
+
+typedef struct {
+    char *module_name;
+    char *signature;
+} FunctionSignature;
+
+void write_byte_to_addr(LPVOID lpAddr, BYTE b);
+void write_dword_to_addr(LPVOID lpAddr, DWORD dw);
+BOOL write_jmp_instruction_to_addr(LPVOID lpOrgProc, LPVOID lpNewProc);
+BOOL find_signature(FunctionSignature *sig, LPVOID *address, char **error);
+BOOL find_signature_in_module(FunctionSignature *sig, const char *module_name, LPVOID *address, char **error);
+BOOL override_function_by_signature(FunctionSignature *sig, LPVOID replacement, LPVOID *patched_address, char **error);
+BOOL override_function_by_signature_in_module(FunctionSignature *sig, const char *module_name, LPVOID replacement, LPVOID *patched_address, char **error);
---
+++ VisualSession.cs
@@ -0,0 +1,238 @@
+/**
+ * Copyright (C) 2006  Ole André Vadla Ravnås <oleavr@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace oSpy
+{
+    [Serializable()]
+    public class VisualSession
+    {
+        private IPEndpoint localEndpoint;
+        public IPEndpoint LocalEndpoint
+        {
+            get { return localEndpoint; }
+        }
+
+        private IPEndpoint remoteEndpoint;
+        public IPEndpoint RemoteEndpoint
+        {
+            get { return remoteEndpoint; }
+        }
+
+        private List<VisualTransaction> transactions;
+        public List<VisualTransaction> Transactions
+        {
+            get { return transactions; }
+        }
+
+        public VisualSession(IPSession session)
+        {
+            localEndpoint = session.LocalEndpoint;
+            remoteEndpoint = session.RemoteEndpoint;
+            transactions = new List<VisualTransaction>();
+        }
+
+        public void TransactionsCreated()
+        {
+            foreach (VisualTransaction transaction in transactions)
+            {
+                transaction.TransactionsCreated();
+            }
+        }
+
+        public void SessionsCreated()
+        {
+            foreach (VisualTransaction transaction in transactions)
+            {
+                transaction.SessionsCreated();
+            }
+        }
+    }
+
+    public abstract class SessionVisualizer
+    {
+        public virtual bool Visible
+        {
+            get { return true; }
+        }
+
+        public virtual string Name
+        {
+            get
+            {
+                return this.GetType().Name;
+            }
+        }
+
+        public abstract VisualTransaction[] GetTransactions(IPSession session);
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+
+    public class TCPEventsVisualizer : SessionVisualizer
+    {
+        public override string Name
+        {
+            get
+            {
+                return "TCPEvents";
+            }
+        }
+
+        public override bool Visible
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public override VisualTransaction[] GetTransactions(IPSession session)
+        {
+            List<VisualTransaction> transactions = new List<VisualTransaction>(session.Events.Count);
+
+            foreach (TCPEvent ev in session.Events)
+            {
+                VisualTransaction transaction = new VisualTransaction(0, ev.Timestamp);
+                transaction.BodyText = ev.ToString();
+                transactions.Add(transaction);
+            }
+
+            return transactions.ToArray();
+        }
+    }
+
+    public class TCPVisualizer : SessionVisualizer
+    {
+        public override string Name
+        {
+            get
+            {
+                return "TCP";
+            }
+        }
+
+        private const int maxPreview = 128;
+
+        public override VisualTransaction[] GetTransactions(IPSession session)
+        {
+            List<VisualTransaction> transactions = new List<VisualTransaction>();
+
+            session.ResetState();
+
+            PacketStream stream1 = session.GetNextStreamDirection();
+            PacketStream stream2 = session.GetNextStreamDirection();
+
+            List<IPPacket> packets = new List<IPPacket>(stream1.Packets.Count + stream2.Packets.Count);
+            packets.AddRange(stream1.Packets);
+            packets.AddRange(stream2.Packets);
+            packets.Sort();
+
+            VisualTransaction transaction = null;
+            MemoryStream previewData = new MemoryStream();
+            int transferred = 0;
+
+            foreach (IPPacket packet in packets)
+            {
+                if (transaction == null || (transaction != null && packet.Direction != transaction.Direction))
+                {
+                    if (transaction != null)
+                    {
+                        transaction.AddHeaderField("Size", transferred);
+
+                        byte[] bytes = previewData.ToArray();
+                        transaction.SetBodyFromTruncatedPreviewData(bytes, transferred - bytes.Length);
+                    }
+
+                    transaction = new VisualTransaction(packet.Index, packet.Direction, packet.Timestamp);
+                    transaction.AddHeaderField("Direction", packet.Direction);
+                    transaction.HeaderRowsPerCol = 1;
+                    transactions.Add(transaction);
+                    previewData.SetLength(0);
+                    transferred = 0;
+                }
+
+                transaction.EndTime = packet.Timestamp;
+
+                if (transferred < maxPreview)
+                {
+                    int n = packet.Bytes.Length;
+                    if (transferred + n > maxPreview)
+                        n = maxPreview - transferred;
+
+                    previewData.Write(packet.Bytes, 0, n);
+                }
+
+                transferred += packet.Bytes.Length;
+            }
+
+            if (transaction != null)
+            {
+                transaction.AddHeaderField("Size", transferred);
+
+                byte[] bytes = previewData.ToArray();
+                transaction.SetBodyFromTruncatedPreviewData(bytes, transferred - bytes.Length);
+            }
+
+            return transactions.ToArray();
+        }
+    }
+
+    public class StreamVisualizationManager
+    {
+        protected static List<SessionVisualizer> visualizers;
+        protected static Dictionary<string, SessionVisualizer> visualizerFromName;
+        public static SessionVisualizer[] Visualizers
+        {
+            get { return visualizers.ToArray(); }
+        }
+
+        protected static SessionVisualizer tcpEventsVis;
+        public static SessionVisualizer TCPEventsVis
+        {
+            get { return tcpEventsVis; }
+        }
+
+        static StreamVisualizationManager()
+        {
+            // FIXME: get these from plugins
+
+            visualizers = new List<SessionVisualizer>();
+            visualizerFromName = new Dictionary<string, SessionVisualizer>();
+
+            tcpEventsVis = new TCPEventsVisualizer();
+            Register(tcpEventsVis);
+
+            Register(new TCPVisualizer());
+            Register(new MSNP2PVisualizer());
+        }
+
+        private static void Register(SessionVisualizer visualizer)
+        {
+            visualizers.Add(visualizer);
+            visualizerFromName.Add(visualizer.Name, visualizer);
+        }
+    }
+}
