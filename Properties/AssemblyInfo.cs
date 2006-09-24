b379e3359eeb3a9be6a45604a6093ba04a17fe50
oleavr <oleavr@60f4c359-591d-0410-bfb7-312270ef6092>
2006-09-24 02:11:06 +0000

    Bumped to 1.8.2

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.8.1.0")]
-[assembly: AssemblyFileVersion("1.8.1.0")]
+[assembly: AssemblyVersion("1.8.2.0")]
+[assembly: AssemblyFileVersion("1.8.2.0")]
