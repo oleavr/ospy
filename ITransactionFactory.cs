2b004da9c7ede0ef1d1c18b7f3c737f4d8c5df6e
husfro <husfro@60f4c359-591d-0410-bfb7-312270ef6092>
2006-09-28 11:26:04 +0000

    Code cleanup

---
+++ ITransactionFactory.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace oSpy
+{
+    interface ITransactionFactory
+    {
+        bool HandleSession(IPSession session);
+        DebugLogger Logger { get; }
+        string Name();
+    }
+}
