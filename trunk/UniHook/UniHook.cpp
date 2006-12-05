c00dc8feeca1fdff6075aeb10d6ac03b0b3a48fd
oleavr <oleavr@60f4c359-591d-0410-bfb7-312270ef6092>
2006-12-05 02:28:36 +0000

    The ultimate hooking technique is implemented and WORKING! \o/

--- trunk/UniHook/UniHook.cpp
+++ trunk/UniHook/UniHook.cpp
@@ -1,7 +1,23 @@
-// UniHook.cpp : Defines the entry point for the console application.
-//
+/**
+ * Copyright (C) 2006  Ole André Vadla Ravnås <oleavr@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 
 #include "stdafx.h"
+#include "Util.h"
 #include <Imagehlp.h>
 #include <winsock2.h>
 #include <iostream>
@@ -25,10 +41,14 @@ public:
 private:
 	static void Stage2Proxy();
 
-	static void PreExecRedirProxy(LPVOID caller_address) { CHooker::self()->PreExecProxy(caller_address); }
-	void PreExecProxy(LPVOID caller_address);
+	static void PreExecRedirProxy(LPVOID callerAddress) { CHooker::self()->PreExecProxy(callerAddress); }
+	void PreExecProxy(LPVOID callerAddress);
 
-	static void Stage3Proxy() {}
+	static void Stage3Proxy();
+
+	void PostExecProxy(LPVOID callerAddress, DWORD retAddr, LPVOID args, DWORD argsSize, DWORD &retval);
+
+
 
 	map<LPVOID, string> hookedAddrToName;
 };
@@ -60,244 +80,11 @@ CHooker::HookModule(TCHAR *name)
 		char *name = (char *) h + names[i];
 		LPVOID addr = (unsigned char *) h + functions[ordinals[i]];
 
-		if (strcmp(name, "socket") == 0)
-		{
-			HookFunction(name, addr);
-		}
+		HookFunction(name, addr);
 	}
 
 	cout << hookedAddrToName.size() << " out of " << expDir->NumberOfNames << " functions hooked" << endl;
 }
-
-// TAKE 1
-#if 0
-bool
-CHooker::HookFunction(const string &name, LPVOID address)
-{
-	unsigned char sig[] = {
-		0x8B, 0xFF, // mov edi, edi
-		0x55,       // push ebp
-		0x8B, 0xEC, // mov ebp, esp
-	};
-
-	if (memcmp(address, sig, sizeof(sig)) != 0)
-	{
-		cout << "failed to hook function " << name << " at " << address << endl;
-		return false;
-	}
-
-	hookedAddrToName[address] = name;
-
-	unsigned char *proxy = new unsigned char[1 + 5 + 5 + 6 + 1 + sizeof(sig) + 5];
-
-	int offset = 0;
-
-	// PUSHAD
-	proxy[offset++] = 0x60;
-
-	// PUSH <absolute address of the hooked function>
-	proxy[offset++] = 0x68;
-	*((DWORD *) (proxy + offset)) = (DWORD) address;
-	offset += 4;
-
-	// CALL near, relative to the common proxy
-	proxy[offset++] = 0xE8;
-	*((DWORD *) (proxy + offset)) = (DWORD) &ProxyFunction - (DWORD) (proxy + offset + 4);
-	offset += 4;
-
-	// Clean up after the call (cdecl)
-	proxy[offset++] = 0x81;			   // ADD
-	proxy[offset++] = 0xC4;			   // ESP
-	*((DWORD *) (proxy + offset)) = 4; // 4
-	offset += 4;
-
-	// POPAD
-	proxy[offset++] = 0x61;
-
-	// Do what the original function did where we overwrote it
-	memcpy(proxy + offset, sig, sizeof(sig));
-	offset += sizeof(sig);
-
-	// JMP to after that part
-	proxy[offset++] = 0xE9;
-	*((DWORD *) (proxy + offset)) = ((DWORD) address + sizeof(sig)) - (DWORD) (proxy + offset + 4);
-	offset += 4;
-
-	// Time to patch
-	unsigned char buf[5];
-	buf[0] = 0xE9;
-	*((DWORD *) (buf + 1)) = (DWORD) proxy - (DWORD) address - sizeof(buf);
-
-	HANDLE process;
-	DWORD oldProtect, nWritten;
-
-	process = GetCurrentProcess();
-	VirtualProtect(address, sizeof(buf), PAGE_EXECUTE_WRITECOPY, &oldProtect);
-	WriteProcessMemory(process, address, buf, sizeof(buf), &nWritten);
-	FlushInstructionCache(process, NULL, 0);
-
-	return true;
-}
-#endif
-
-// TAKE 2
-#if 0
-	//
-	// STEP 1: Save all registers
-	//
-
-
-	//
-	// STEP 2: We need to make a copy of the stack frame.
-	//
-	// At this point the stack looks like this:
-	//
-	//    [  .....  ]  |
-	//    [  arg_n  ]  |
-	//    [  .....  ]  |
-	//    [  arg_0  ]  |
-	//    [ retaddr ] \|/
-	//    [  sregs  ]  V
-	//
-    // And we make it look like this:
-    //
-	//    [  .....  ]  |
-	//    [  arg_n  ]  |
-	//    [  .....  ]  |
-	//    [  arg_0  ]  |
-	//    [ retaddr ]  |
-	//    [  sregs  ]  |
-	//    [  .....  ]  |
-	//    [  arg_n  ]  |
-	//    [  .....  ]  |
-	//    [  arg_0  ] \|/
-	//    [ retaddr ]  V
-    //
-	// Why are we doing this?
-	//   1) Because the API function might use any of the arguments' stack
-	//      space for temporary storage when it's done with them, we make
-	//      a copy of the whole thing so that the function won't clobber
-	//      the original arguments.
-	//   2) We want to conveniently trap the return.
-	//
-	//      This is also the perfect time to figure out how many bytes of
-	//      arguments the function actually consumed as API functions in
-	//      general are stdcall (diff == 0 => cdecl).  We figure this out
-	//      by taking ESP_before, which we save in ESI prior to calling
-	//      the function, and compare it to ESP_now. The size of the
-	//      arguments will be ESP_now - ESP_before - sizeof(LPVOID),
-	//      where the latter is the return address.
-	//
-	// NOTE:
-	// The number of bytes to copy could be determined heuristically
-	// by walking up the stack looking for valid pointers and finding
-	// one pointing to an instruction following a CALL (to find the
-	// previous stack frame), but this is kind of risky so we'll instead
-	// copy a "safe" number of bytes and hope we got it all (and leave
-	// the number of bytes tuneable).
-	//
-
-	memcpy(proxy + offset, templ_copy_stack_frame, sizeof(templ_copy_stack_frame));
-	*((DWORD *) (proxy + offset + 1)) = MAX_FRAME_SIZE / 4;
-	*((DWORD *) (proxy + offset + 7)) = MAX_FRAME_SIZE;
-	*((DWORD *) (proxy + offset + 20)) = MAX_FRAME_SIZE;
-	offset += sizeof(templ_copy_stack_frame);
-
-
-	//
-	// STEP 3: Call the pre-execution proxy
-	//
-
-	// push <absolute address of the hooked function>
-	proxy[offset++] = 0x68;
-	*((DWORD *) (proxy + offset)) = (DWORD) address;
-	offset += 4;
-
-	// call
-	proxy[offset++] = 0xE8;
-	*((DWORD *) (proxy + offset)) = (DWORD) &ProxyFunction - (DWORD) (proxy + offset + 4);
-	offset += 4;
-
-	// clean up after the call (cdecl)
-	proxy[offset++] = 0x81;			   // ADD
-	proxy[offset++] = 0xC4;			   // ESP
-	*((DWORD *) (proxy + offset)) = 4; // 4
-	offset += 4;
-
-
-	//
-	// STEP 4: Overwrite the return address on the copy so that the API
-    //         function returns to our stage 2 proxy instead of the
-	//         caller.
-	//
-
-	// mov [esp], offsetof STEPX
-	proxy[offset++] = 0xc7;
-	proxy[offset++] = 0x04;
-	proxy[offset++] = 0x24;
-	*((DWORD *) (proxy + offset)) = 0; // FIXME
-	offset += 4;
-
-
-	//
-	// STEP 5: Save ESP in ESI.
-	//
-
-	// mov esi, esp
-	proxy[offset++] = 0x89;
-	proxy[offset++] = 0xe6;
-
-
-	//
-	// STEP 6: Do what the original function did where we overwrote it and JMP
-	//         to the next instruction thereafter.
-	//
-
-	// The instructions that we overwrote
-	memcpy(proxy + offset, sig, sizeof(sig));
-	offset += sizeof(sig);
-
-	// And finally JMP to the next instruction in the original
-	proxy[offset++] = 0xE9;
-	*((DWORD *) (proxy + offset)) = ((DWORD) address + sizeof(sig)) - (DWORD) (proxy + offset + 4);
-	offset += 4;
-
-	//
-	// STEP 7: The API function has returned to us.
-	//
-
-
-	//
-	// PATCH THE START OF THE ORIGINAL FUNCTION TO BOUNCE TO THE START OF THE
-	// PROXY *DRUMROLL*
-	//
-	unsigned char buf[5];
-	buf[0] = 0xE9;
-	*((DWORD *) (buf + 1)) = (DWORD) proxy - (DWORD) address - sizeof(buf);
-
-	HANDLE process;
-	DWORD oldProtect, nWritten;
-
-	process = GetCurrentProcess();
-	VirtualProtect(address, sizeof(buf), PAGE_EXECUTE_WRITECOPY, &oldProtect);
-	WriteProcessMemory(process, address, buf, sizeof(buf), &nWritten);
-	FlushInstructionCache(process, NULL, 0);
-
-	return true;
-
-static const unsigned char templ_copy_stack_frame[] = {
-	0xb9, 0xff, 0xff, 0xff, 0xff,       // <00> mov ecx, (MAX_FRAME_SIZE / 4)
-	0x81, 0xec, 0xff, 0xff, 0xff, 0xff, // <05> sub esp, MAX_FRAME_SIZE
-	0x56,                               // <11> push esi
-	0x57,                               // <12> push edi
-	0xfc,                               // <13> cld
-	0x8d, 0x7c, 0x24, 0x08,             // <14> lea edi, [esp + 8]
-	0x8d, 0xb7, 0xff, 0xff, 0xff, 0xff, // <18> lea esi, [edi + MAX_FRAME_SIZE]
-	0xf3, 0xa5,                         // <24> rep movsd
-	0x5f,                               // <26> pop edi
-	0x5e                                // <27> pop esi
-};
-#endif
 
 bool
 CHooker::HookFunction(const string &name, LPVOID address)
@@ -393,18 +180,21 @@ CHooker::HookFunction(const string &name, LPVOID address)
 }
 
 #define MAX_FRAME_SIZE  4096
-#define SAVED_REGS_SIZE   32
+#define SAVED_REGS_SIZE   16
 
 __declspec (naked) void
 CHooker::Stage2Proxy()
 {
 	__asm {
 		//
-		// STEP 1: Save all registers and store the dynamic proxy's address
-		//         somewhere safe.
+		// STEP 1: Save all registers that have to be left unclobbered, and
+		//         store the dynamic proxy's address somewhere safe.
 		//
 
-		pushad;
+		push ebx;
+		push ebp;
+		push esi;
+		push edi;
 
 		mov esi, ecx;
 
@@ -506,9 +296,83 @@ CHooker::Stage2Proxy()
 }
 
 void
-CHooker::PreExecProxy(LPVOID caller_address)
+CHooker::PreExecProxy(LPVOID callerAddress)
 {
-	cout << "PreExecProxy: called from " << this->hookedAddrToName[caller_address] << " (" <<  caller_address << ")" << endl;
+	cout << "PreExecProxy: called from " << this->hookedAddrToName[callerAddress] << " (" <<  callerAddress << ")" << endl;
+}
+
+__declspec (naked) void
+CHooker::Stage3Proxy()
+{
+	LPVOID callerAddress, args;
+	DWORD retAddr, argsSize, retval;
+
+	__asm {
+		// Calculate the number of bytes of arguments consumed
+		mov ecx, esp;
+		sub ecx, edi;
+
+		// And then clear off the stack so that it's back to the point where it was
+		// after stage 2 step 1 (registers saved).
+		mov ebx, MAX_FRAME_SIZE;
+		sub ebx, 4;
+		sub ebx, ecx;
+		add esp, ebx;
+
+		// Do the prolog dance
+		push ebp;
+		mov ebp, esp;
+		sub esp, __LOCAL_SIZE;
+
+		mov [argsSize], ecx;
+		mov [retval], eax;
+
+		mov edx, [esi];
+		mov [callerAddress], edx;
+
+		mov esi, ecx
+
+		lea ecx, [ebp + 4 + SAVED_REGS_SIZE];
+		mov edx, [ecx];
+		mov [retAddr], edx;
+
+		add ecx, 4;
+		mov [args], ecx;
+	}
+
+	CHooker::self()->PostExecProxy(callerAddress, retAddr, args, argsSize, retval);
+
+	__asm {
+		// Just in case we want to change the return value
+		mov eax, [retval];
+
+		// And the epilog dance
+		mov esp, ebp;
+		pop ebp;
+
+		mov ecx, esi;
+
+		// Restore registers
+		pop edi;
+		pop esi;
+		pop ebp;
+		pop ebx;
+
+		// Now we have to clear the stack like the original function would have
+		mov edx, dword ptr [esp] // return address
+		add esp, 4;    // clear off the return address
+		add esp, ecx;  // ...and the arguments
+		jmp edx;       // jump back to the caller
+	}
+}
+
+void
+CHooker::PostExecProxy(LPVOID callerAddress, DWORD retAddr, LPVOID args, DWORD argsSize, DWORD &retval)
+{
+	cout << "PostExecProxy: " << this->hookedAddrToName[callerAddress] << " (" <<  callerAddress << ") called with retAddr=" << retAddr << ", argsSize=" << argsSize << " and retval=" << (int) retval << endl;
+
+	if (argsSize > 0)
+		cout << "PostExecProxy:" << endl << hexdump(args, argsSize, 16);
 }
 
 typedef SOCKET (__stdcall *SocketFunc) (int af, int type, int protocol);
@@ -519,8 +383,17 @@ int _tmain(int argc, _TCHAR* argv[])
 
 	CHooker::self()->HookModule(L"C:\\WINDOWS\\SYSTEM32\\ws2_32.dll");
 
+	WORD versionRequested = MAKEWORD(2, 2);
+	WSADATA wsaData;
+
+	WSAStartup(versionRequested, &wsaData);
+
 	SocketFunc socketFunc = (SocketFunc) GetProcAddress(h, "socket");
 	int s = socketFunc(AF_INET, SOCK_STREAM, 0);
+
+	cout << "socket() returned " << s << endl;
+
+	WSACleanup();
 
 	return 0;
 }
