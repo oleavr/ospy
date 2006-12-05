37d21d865a13cd7100cf885b61ce3c2729108edf
oleavr <oleavr@60f4c359-591d-0410-bfb7-312270ef6092>
2006-12-05 21:36:13 +0000

    - Use a custom allocator that calls HeapAlloc/HeapFree for STL.classes so that the code can be merged into oSpy.

--- trunk/UniHook/Util.cpp
+++ trunk/UniHook/Util.cpp
@@ -23,36 +23,6 @@
 #include <sstream>
 #include <iomanip>
 
-void *
-ospy_malloc(size_t size)
-{
-    return HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, size);
-}
-
-void *
-ospy_realloc(void *ptr, size_t new_size)
-{
-    return HeapReAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, ptr, new_size);
-}
-
-void
-ospy_free(void *ptr)
-{
-    HeapFree(GetProcessHeap(), 0, ptr);
-}
-
-char *
-ospy_strdup(const char *str)
-{
-    char *s;
-    size_t size = strlen(str) + 1;
-
-    s = (char *) ospy_malloc(size);
-    memcpy(s, str, size);
-
-    return s;
-}
-
 void
 get_module_name_for_address(LPVOID address,
                             char *buf, int buf_size)
