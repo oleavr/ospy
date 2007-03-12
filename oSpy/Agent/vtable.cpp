dd4c333ba8b4a8b6492028182c29183238b10da4
oleavr <oleavr@60f4c359-591d-0410-bfb7-312270ef6092>
2007-03-12 22:58:48 +0000

    Some more work towards a useful vtable hooking framework. Currently broken.

--- oSpy/Agent/vtable.cpp
+++ oSpy/Agent/vtable.cpp
@@ -28,40 +28,40 @@ VTableHooker::Self()
 }
 
 void
-VTableHooker::HookVTableAt(void *startOffset, int numFuncs)
+VTableHooker::HookVTable(VTableSpec &vtable)
 {
-	DWORD *vtable = (DWORD *) startOffset;
 	DWORD oldProtect;
+	VirtualProtect((LPVOID) vtable.GetStartOffset(), vtable.GetMethodCount() * sizeof(LPVOID),
+		PAGE_READWRITE, &oldProtect);
 
-	VirtualProtect(startOffset, numFuncs * sizeof(DWORD), PAGE_EXECUTE_WRITECOPY, &oldProtect);
+	DWORD *methods = (DWORD *) vtable.GetStartOffset();
 
-	for (int i = 0; i < numFuncs; i++)
+	for (int i = 0; i < vtable.GetMethodCount(); i++)
 	{
-		unsigned char *trampoline = new unsigned char[5 + sizeof(VFuncContext)];
+		unsigned char *trampoline = new unsigned char[5 + sizeof(VMethodSpec *)];
 		int offset = 0;
 
 		trampoline[offset++] = 0xe8;
 		*((DWORD *) (trampoline + offset)) = (DWORD) VTableHooker::VTableProxyFunc - (DWORD) (trampoline + offset + 4);
 		offset += sizeof(DWORD);
 
-		VFuncContext *ctx = (VFuncContext *) (trampoline + offset);
-		ctx->index = i;
-		ctx->functionStart = vtable[i];
+		*((VMethodSpec **) (trampoline + offset)) = &vtable[i];
 
-		vtable[i] = (DWORD) trampoline;
+		methods[i] = (DWORD) trampoline;
 	}
 }
 
 __declspec(naked) void
-VTableHooker::VTableProxyFunc(CpuContext cpuCtx, VFuncContext *funcCtx)
+VTableHooker::VTableProxyFunc(CpuContext cpuCtx, VMethodSpec *methodSpec)
 {
 	__asm {
 		sub esp, 4;
 		push eax;
 		push ebx;
 		mov eax, [esp+12];
-		mov ebx, eax;
-		mov eax, [eax+4];
+		mov eax, [eax]; // dereference VMethodSpec **
+		mov ebx, eax; // VMethodSpec *
+		mov eax, [eax+VMethodSpec::m_offset];
 		mov [esp+12], eax;
 		mov [esp+8], ebx;
 		pop ebx;
@@ -75,7 +75,9 @@ VTableHooker::VTableProxyFunc(CpuContext cpuCtx, VFuncContext *funcCtx)
 		sub esp, __LOCAL_SIZE;
 	}
 
-	message_logger_log_message("VTableProxyFunc", 0, MESSAGE_CTX_INFO, "index=%d, functionStart=0x%08x", funcCtx->index, funcCtx->functionStart);
+	message_logger_log_message("VTableProxyFunc", 0, MESSAGE_CTX_INFO,
+		"Index=%d, Offset=0x%08x, Name=%s",
+		methodSpec->GetIndex(), methodSpec->GetOffset(), methodSpec->GetName().c_str());
 
 	__asm {
 		mov esp, ebp;
@@ -87,4 +89,26 @@ VTableHooker::VTableProxyFunc(CpuContext cpuCtx, VFuncContext *funcCtx)
 		add esp, 4;
 		ret;
 	}
+}
+
+VTableSpec::VTableSpec(const OString &name, DWORD startOffset, int methodCount)
+	: m_name(name), m_startOffset(startOffset), m_methods(methodCount)
+{
+	DWORD *funcs = (DWORD *) startOffset;
+
+	for (int i = 0; i < methodCount; i++)
+	{
+		m_methods[i].Initialize(this, i, funcs[i]);
+	}
+}
+
+void
+VMethodSpec::Initialize(VTableSpec *vtable, int index, DWORD offset)
+{
+	m_vtable = vtable;
+	m_index = index;
+	m_offset = offset;
+
+	m_name = "Method_";
+	m_name += index;
 }
--- oSpy/Agent/vtable.h
+++ oSpy/Agent/vtable.h
@@ -7,13 +7,51 @@ typedef struct {
 	DWORD functionStart;
 } VFuncContext;
 
-class VTableHooker : BaseObject
+class VTableSpec;
+class VMethodSpec;
+
+class VTableHooker : public BaseObject
 {
 public:
 	static VTableHooker *Self();
 
-	void HookVTableAt(void *startOffset, int numFuncs);
+	void HookVTable(VTableSpec &vtable);
 
 protected:
-	static void VTableProxyFunc(CpuContext cpuCtx, VFuncContext *funcCtx);
+	static void VTableProxyFunc(CpuContext cpuCtx, VMethodSpec *methodSpec);
+};
+
+class VTableSpec : public BaseObject
+{
+public:
+	VTableSpec(const OString &name, DWORD startOffset, int methodCount);
+
+	const OString &GetName() { return m_name; }
+	DWORD GetStartOffset() { return m_startOffset; }
+	int GetMethodCount() { return m_methods.size(); }
+	VMethodSpec *GetMethodByIndex(int index) { return &m_methods[index]; }
+
+	VMethodSpec &operator[](int index) { return m_methods[index]; }
+protected:
+	OString m_name;
+	DWORD m_startOffset;
+	OVector<VMethodSpec>::Type m_methods;
+};
+
+class VMethodSpec : public BaseObject
+{
+public:
+	void Initialize(VTableSpec *vtable, int index, DWORD offset);
+
+	const VTableSpec *GetVTable() { return m_vtable; }
+	int GetIndex() { return m_index; }
+	DWORD GetOffset() { return m_offset; }
+	const OString &GetName() { return m_name; }
+	void SetName(const OString &name) { m_name = name; }
+
+protected:
+	VTableSpec *m_vtable;
+	int m_index;
+	DWORD m_offset;
+	OString m_name;
 };
