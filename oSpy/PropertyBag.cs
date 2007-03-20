a4fc48fddb5996932e4d5616bf1b8ac77d160007
oleavr <oleavr@60f4c359-591d-0410-bfb7-312270ef6092>
2007-03-20 00:15:08 +0000

    Switched the rest of the code from GPL to MIT.

--- oSpy/PropertyBag.cs
+++ oSpy/PropertyBag.cs
@@ -1,27 +1,28 @@
-/********************************************************************
- *
- *  PropertyBag.cs
- *  --------------
- *  Copyright (C) 2002  Tony Allowatt
- *  Last Update: 12/14/2002
- * 
- *  THE SOFTWARE IS PROVIDED BY THE AUTHOR "AS IS", WITHOUT WARRANTY
- *  OF ANY KIND, EXPRESS OR IMPLIED. IN NO EVENT SHALL THE AUTHOR BE
- *  LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY ARISING FROM,
- *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OF THIS
- *  SOFTWARE.
- * 
- *  Public types defined in this file:
- *  ----------------------------------
- *  namespace Flobbster.Windows.Forms
- *     class PropertySpec
- *     class PropertySpecEventArgs
- *     delegate PropertySpecEventHandler
- *     class PropertyBag
- *        class PropertyBag.PropertySpecCollection
- *     class PropertyTable
- *
- ********************************************************************/
+//
+// Copyright (c) 2002 Tony Allowatt
+// Copyright (c) 2006 Ole André Vadla Ravnås <oleavr@gmail.com>
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+//
 
 using System;
 using System.Collections;
