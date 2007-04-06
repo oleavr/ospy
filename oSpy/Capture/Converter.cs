c7ccd6d566eff0c0498526ec4f33c83dccb3f6c5
oleavr <oleavr@60f4c359-591d-0410-bfb7-312270ef6092>
2007-04-06 20:05:12 +0000

    Merge the traces and write to an XML file.

--- oSpy/Capture/Converter.cs
+++ oSpy/Capture/Converter.cs
@@ -1,5 +1,5 @@
 ﻿//
-// Copyright (c) 2006 Ole André Vadla Ravnås <oleavr@gmail.com>
+// Copyright (c) 2007 Ole André Vadla Ravnås <oleavr@gmail.com>
 //
 // Permission is hereby granted, free of charge, to any person
 // obtaining a copy of this software and associated documentation
@@ -27,34 +27,111 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.IO;
+using System.Xml;
+using oSpy.Util;
 
 namespace oSpy.Capture
 {
     class Converter
     {
-        public void ConvertAll(string captureDirPath, IProgressFeedback progress)
+        public void ConvertAll(string captureDirPath, int numEvents, IProgressFeedback progress)
         {
-            Dictionary<uint, uint> ids = new Dictionary<uint, uint>();
+            SortedList<uint, KeyValuePair<BinaryReader, uint>> ids = new SortedList<uint, KeyValuePair<BinaryReader, uint>>(numEvents);
 
+            uint i = 0;
             foreach (string filePath in Directory.GetFiles(captureDirPath, "*.log", SearchOption.TopDirectoryOnly))
             {
-                progress.ProgressUpdate(String.Format("Converting '{0}'", filePath), 100);
-
                 FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read);
                 BinaryReader r = new BinaryReader(fs);
 
                 while (fs.Position < fs.Length)
                 {
+                    i++;
+                    int pct = (int)(((float)i / (float)numEvents) * 100.0f);
+                    progress.ProgressUpdate("Indexing", pct);
+
                     uint id = r.ReadUInt32();
                     uint size = r.ReadUInt32();
-                    ids[id] = (uint) fs.Position + 4;
+
+                    ids.Add(id, new KeyValuePair<BinaryReader, uint>(r, (uint)fs.Position));
 
                     fs.Seek(size, SeekOrigin.Current);
                 }
-
-                r.Close();
-                fs.Close();
             }
+
+            string resultPath = String.Format("{0}\\capture.xml", captureDirPath);
+            XmlTextWriter xtw = new XmlTextWriter(resultPath, System.Text.Encoding.UTF8);
+            xtw.Formatting = Formatting.Indented;
+            xtw.Indentation = 4;
+            xtw.IndentChar = ' ';
+            xtw.WriteStartDocument(true);
+            xtw.WriteStartElement("Events");
+
+            i = 0;
+            foreach (KeyValuePair<BinaryReader, uint> pair in ids.Values)
+            {
+                i++;
+                int pct = (int)(((float)i / (float)numEvents) * 100.0f);
+                progress.ProgressUpdate(String.Format("Converting event {0} of {1}", i, numEvents), pct);
+
+                BinaryReader r = pair.Key;
+                uint offset = pair.Value;
+
+                r.BaseStream.Seek(offset, SeekOrigin.Begin);
+                UnserializeNode(r, xtw);
+            }
+
+            xtw.WriteEndElement();
+            xtw.WriteEndDocument();
+            xtw.Close();
+        }
+
+        private void UnserializeNode(BinaryReader r, XmlTextWriter xtw)
+        {
+            xtw.WriteStartElement(UnserializeString(r));
+
+            uint attrCount = r.ReadUInt32();
+            for (int i = 0; i < attrCount; i++)
+            {
+                xtw.WriteAttributeString(UnserializeString(r), UnserializeString(r));
+            }
+
+            UInt32 contentIsRaw = r.ReadUInt32();
+            string content = "";
+
+            if (contentIsRaw != 0)
+            {
+                uint len = r.ReadUInt32();
+                byte[] bytes = r.ReadBytes((int) len);
+                if (bytes.Length > 0)
+                {
+                    content = Convert.ToBase64String(bytes, Base64FormattingOptions.None);
+                }
+            }
+            else
+            {
+                content = UnserializeString(r);
+            }
+
+            if (content != String.Empty)
+            {
+                xtw.WriteString(content);
+            }
+
+            uint childCount = r.ReadUInt32();
+            for (int i = 0; i < childCount; i++)
+            {
+                UnserializeNode(r, xtw);
+            }
+
+            xtw.WriteEndElement();
+        }
+
+        private string UnserializeString(BinaryReader r)
+        {
+            uint len = r.ReadUInt32();
+            byte[] buf = r.ReadBytes((int) len);
+            return StaticUtils.DecodeASCII(buf);
         }
     }
 }
