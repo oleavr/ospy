1ee3468a63e75d8fbba97720392f580294e7d203
oleavr <oleavr@60f4c359-591d-0410-bfb7-312270ef6092>
2007-04-12 13:30:56 +0000

    - Try to make the coding style consistent. - Match the columns in the new format.

--- oSpyViewer/MainWindow.cs
+++ oSpyViewer/MainWindow.cs
@@ -1,43 +1,36 @@
 using System;
 using Gtk;
 
-public class MainWindow: Gtk.Window
+public class MainWindow : Gtk.Window
 {
     protected Gtk.TreeView EventList;
 
-    
-    public MainWindow (): base ("")
+    public MainWindow(): base("")
     {
-        Stetic.Gui.Build (this, typeof(MainWindow));
-        // index, type, time, FunctionName, ReturnAddress, Sender, Description, Comment
+        Stetic.Gui.Build(this, typeof(MainWindow));
+        // index, type, time, sender, description
         Gtk.TreeStore EventListStore = new Gtk.TreeStore(typeof(int),
-                                                              typeof(Gdk.Pixbuf),
-                                                              typeof(string),
-                                                              typeof(string),
-                                                              typeof(string),
-                                                              typeof(string),
-                                                              typeof(string),
-                                                              typeof(string));
-        EventList.AppendColumn("Index", new Gtk.CellRendererText (), "text", 0);
-        EventList.AppendColumn("Type", new Gtk.CellRendererPixbuf (), "pixbuf", 1);
-        EventList.AppendColumn("Time", new Gtk.CellRendererText (), "text", 2);
-        EventList.AppendColumn("Function Name", new Gtk.CellRendererText (), "text", 3);
-        EventList.AppendColumn("Return Address", new Gtk.CellRendererText (), "text", 4);
-        EventList.AppendColumn("Sender", new Gtk.CellRendererText (), "text", 5);
-        EventList.AppendColumn("Description", new Gtk.CellRendererText (), "text", 6);
-        EventList.AppendColumn("Comment", new Gtk.CellRendererText (), "text", 7);
-        
+                                                         typeof(Gdk.Pixbuf),
+                                                         typeof(string),
+                                                         typeof(string),
+                                                         typeof(string));
+        EventList.AppendColumn("Index", new Gtk.CellRendererText(), "text", 0);
+        EventList.AppendColumn("Type", new Gtk.CellRendererPixbuf(), "pixbuf", 1);
+        EventList.AppendColumn("Time", new Gtk.CellRendererText(), "text", 2);
+        EventList.AppendColumn("Sender", new Gtk.CellRendererText(), "text", 3);
+        EventList.AppendColumn("Description", new Gtk.CellRendererText(), "text", 4);
+
         EventList.Model = EventListStore;
     }
     
-    protected void OnDeleteEvent (object sender, DeleteEventArgs a)
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
     {
-        Application.Quit ();
+        Application.Quit();
         a.RetVal = true;
     }
 
     protected virtual void OnQuit(object sender, System.EventArgs e)
     {
-        Application.Quit ();
+        Application.Quit();
     }
 }
