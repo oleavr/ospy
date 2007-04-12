ac078eb3dad35ae91f949b7bdca05c4634f97ec6
Ali.Sabil <Ali.Sabil@60f4c359-591d-0410-bfb7-312270ef6092>
2007-04-12 10:30:15 +0000

    Started a gtk# based viewer : oSpyViewer

---
+++ trunk/oSpyViewer/Main.cs
@@ -0,0 +1,17 @@
+// project created on 4/12/2007 at 1:07 AM
+using System;
+using Gtk;
+
+namespace oSpyViewer
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Application.Init ();
+			MainWindow win = new MainWindow ();
+			win.Show ();
+			Application.Run ();
+		}
+	}
+}
