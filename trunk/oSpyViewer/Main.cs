ce0a16838dde3d3eff53ea2e23aa1d1035d3b20e
oleavr <oleavr@60f4c359-591d-0410-bfb7-312270ef6092>
2007-04-12 13:11:21 +0000

    s/\t/    /g

--- trunk/oSpyViewer/Main.cs
+++ trunk/oSpyViewer/Main.cs
@@ -4,14 +4,14 @@ using Gtk;
 
 namespace oSpyViewer
 {
-	class MainClass
-	{
-		public static void Main (string[] args)
-		{
-			Application.Init ();
-			MainWindow win = new MainWindow ();
-			win.Show ();
-			Application.Run ();
-		}
-	}
+    class MainClass
+    {
+        public static void Main (string[] args)
+        {
+            Application.Init();
+            MainWindow win = new MainWindow();
+            win.Show();
+            Application.Run();
+        }
+    }
 }
