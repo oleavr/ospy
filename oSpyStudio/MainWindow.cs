3bc0ec5f1d0bdece946044d4698536aa68b7921b
johann.prieur <johann.prieur@60f4c359-591d-0410-bfb7-312270ef6092>
2007-04-14 15:55:49 +0000

    Work on the UI.

--- oSpyStudio/MainWindow.cs
+++ oSpyStudio/MainWindow.cs
@@ -3,12 +3,22 @@ using Gtk;
 
 public class MainWindow : Gtk.Window
 {
-    protected Gtk.TreeView eventList;
+    protected Gtk.TreeView transactionList;
+    protected Gtk.TreeView resourceList;
+    protected Gtk.TreeView processList;
 
     public MainWindow()
         : base("")
     {
         Stetic.Gui.Build(this, typeof(MainWindow));
+
+		Gtk.TreeStore processListStore = new Gtk.TreeStore(typeof(string));
+		processList.Model = processListStore;
+		processList.AppendColumn("Process", new Gtk.CellRendererText(), "text", 0);
+		
+		Gtk.TreeStore resourceListStore = new Gtk.TreeStore(typeof(string));
+		resourceList.Model = resourceListStore;
+		resourceList.AppendColumn("Resource", new Gtk.CellRendererText(), "text", 0);
 
         // index, type, time, sender, description
         Gtk.TreeStore eventListStore = new Gtk.TreeStore(typeof(int),
@@ -16,13 +26,13 @@ public class MainWindow : Gtk.Window
                                                          typeof(string),
                                                          typeof(string),
                                                          typeof(string));
-        eventList.Model = eventListStore;
+        transactionList.Model = eventListStore;
 
-        eventList.AppendColumn("Index", new Gtk.CellRendererText(), "text", 0);
-        eventList.AppendColumn("Type", new Gtk.CellRendererPixbuf(), "pixbuf", 1);
-        eventList.AppendColumn("Time", new Gtk.CellRendererText(), "text", 2);
-        eventList.AppendColumn("Sender", new Gtk.CellRendererText(), "text", 3);
-        eventList.AppendColumn("Description", new Gtk.CellRendererText(), "text", 4);
+        transactionList.AppendColumn("Index", new Gtk.CellRendererText(), "text", 0);
+        transactionList.AppendColumn("Type", new Gtk.CellRendererPixbuf(), "pixbuf", 1);
+        transactionList.AppendColumn("Time", new Gtk.CellRendererText(), "text", 2);
+        transactionList.AppendColumn("Sender", new Gtk.CellRendererText(), "text", 3);
+        transactionList.AppendColumn("Description", new Gtk.CellRendererText(), "text", 4);
     }
     
     protected void OnDeleteEvent(object sender, DeleteEventArgs a)
@@ -34,5 +44,41 @@ public class MainWindow : Gtk.Window
     protected virtual void OnQuit(object sender, System.EventArgs e)
     {
         Application.Quit();
+    }
+    
+    protected virtual void OnOpen(object sender, System.EventArgs e)
+    {
+    	Gtk.FileChooserDialog fc=
+        new Gtk.FileChooserDialog("Choose the file to open",
+        	                      this,
+                                  FileChooserAction.Open,
+                                  "Cancel", ResponseType.Cancel,
+                                  "Open", ResponseType.Accept);
+		Gtk.FileFilter ff = new Gtk.FileFilter();
+		ff.AddPattern("*.osd");
+		ff.Name = "oSpy dump files (.osd)";
+		fc.AddFilter(ff);
+
+        if (fc.Run() == (int)ResponseType.Accept) 
+        {
+        	System.IO.FileStream file = System.IO.File.OpenRead(fc.Filename);
+ 			//TODO: decode the input file and fill the viewer
+ 			file.Close();
+        }
+        fc.Destroy();
+    }
+
+    protected virtual void OnAbout(object sender, System.EventArgs e)
+    {
+    	Gtk.AboutDialog ad = new Gtk.AboutDialog();
+    	ad.Name = "oSpy Studio";
+    	ad.Website = "http://code.google.com/p/ospy/";
+    	string[] Authors = new string[3];
+    	Authors[0] = "Ole André Vadla Ravnås";
+    	Authors[1] = "Ali Sabil";
+    	Authors[2] = "Johann Prieur";
+    	ad.Authors = Authors;
+    	ad.Run();
+    	ad.Destroy();
     }
 }
