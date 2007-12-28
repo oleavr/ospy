7fd8aef0333f0cdad6bbbcbcca4799a0d045cec4
oleavr <oleavr@60f4c359-591d-0410-bfb7-312270ef6092>
2007-12-28 03:41:16 +0000

    Log URBs to file.

--- trunk/oSpyUsbAgent/Logger.cpp
+++ trunk/oSpyUsbAgent/Logger.cpp
@@ -20,37 +20,62 @@
 #include <ntstrsafe.h>
 
 NTSTATUS
-Logger::Initialize ()
+Logger::Initialize (IO_REMOVE_LOCK * removeLock, const WCHAR * fnSuffix)
 {
-  NTSTATUS status;
+  NTSTATUS status = STATUS_SUCCESS;
+
+  status = IoAcquireRemoveLock (removeLock, this);
+  if (!NT_SUCCESS (status))
+    return status;
+  m_removeLock = removeLock;
 
   UNICODE_STRING logfilePath;
   WCHAR buffer[256];
-  logfilePath.Buffer = buffer;
-  logfilePath.Length = 0;
-  logfilePath.MaximumLength = sizeof (buffer);
+  RtlInitEmptyUnicodeString (&logfilePath, buffer, sizeof (buffer));
 
-  LARGE_INTEGER curTime;
-  KeQuerySystemTime (&curTime);
   status = RtlUnicodeStringPrintf (&logfilePath,
-    L"\\SystemRoot\\oSpyUsbAgent-%I64d.log", curTime.QuadPart);
-  if (!NT_SUCCESS (status))
-    return status;
+    L"\\SystemRoot\\oSpyUsbAgent-%s.log", fnSuffix);
+  if (!NT_SUCCESS (status)) return status;
 
-  OBJECT_ATTRIBUTES attrs;
-  InitializeObjectAttributes (&attrs, &logfilePath, 0, NULL, NULL);
+  m_fileHandle = NULL;
 
-  IO_STATUS_BLOCK ioStatus;
-  status = ZwCreateFile (&m_fileHandle, GENERIC_WRITE, &attrs, &ioStatus,
-    NULL, FILE_ATTRIBUTE_NORMAL, FILE_SHARE_READ, FILE_OVERWRITE_IF,
-    FILE_SYNCHRONOUS_IO_NONALERT, NULL, 0);
-  /*
-  if (NT_SUCCESS (status))
+  KeInitializeEvent (&m_stopEvent, NotificationEvent, FALSE);
+  m_logThread = NULL;
+
+  m_index.QuadPart = 0;
+  KeInitializeSpinLock (&m_indexLock);
+
+  ExInitializeSListHead (&m_items);
+  KeInitializeSpinLock (&m_itemsLock);
+
+  __try
   {
-    unsigned char bom[2] = { 0xff, 0xfe };
-    WriteRaw (bom, sizeof (bom));
+    OBJECT_ATTRIBUTES attrs;
+    InitializeObjectAttributes (&attrs, &logfilePath, 0, NULL, NULL);
+
+    IO_STATUS_BLOCK ioStatus;
+    status = ZwCreateFile (&m_fileHandle, GENERIC_WRITE, &attrs, &ioStatus,
+      NULL, FILE_ATTRIBUTE_NORMAL, FILE_SHARE_READ, FILE_OVERWRITE_IF,
+      FILE_SYNCHRONOUS_IO_NONALERT, NULL, 0);
+    if (!NT_SUCCESS (status)) return status;
+
+    status = PsCreateSystemThread (&m_logThread, THREAD_ALL_ACCESS, NULL,
+      NULL, NULL, LogThreadFuncWrapper, this);
+    if (!NT_SUCCESS (status)) return status;
   }
-  */
+  __finally
+  {
+    if (!NT_SUCCESS (status))
+    {
+      if (m_fileHandle != NULL)
+      {
+        ZwClose (m_fileHandle);
+        m_fileHandle = NULL;
+      }
+
+      IoReleaseRemoveLock (removeLock, this);
+    }
+  }
 
   return status;
 }
@@ -58,55 +83,151 @@ Logger::Initialize ()
 void
 Logger::Shutdown ()
 {
+  KeSetEvent (&m_stopEvent, IO_NO_INCREMENT, FALSE);
+
+  if (m_logThread != NULL)
+  {
+    ZwClose (m_logThread);
+    m_logThread = NULL;
+  }
+}
+
+void
+Logger::LogThreadFunc ()
+{
+  KdPrint (("Logger thread speaking"));
+
+  NTSTATUS status;
+  LARGE_INTEGER timeout;
+
+  timeout.QuadPart = -1000000; // 100 ms
+
+  do
+  {
+    status = KeWaitForSingleObject (&m_stopEvent, Executive, KernelMode,
+      FALSE, &timeout);
+    if (status == STATUS_SUCCESS)
+      break;
+
+    UrbLogEntry * entry = reinterpret_cast <UrbLogEntry *> (
+      ExInterlockedPopEntrySList (&m_items, &m_itemsLock));
+    if (entry == NULL)
+      continue;
+
+    WriteUrbEntry (entry);
+
+    ExFreePool (entry);
+  }
+  while (true);
+
   if (m_fileHandle != NULL)
   {
     ZwClose (m_fileHandle);
     m_fileHandle = NULL;
   }
+
+  IoReleaseRemoveLock (m_removeLock, this);
+
+  KdPrint (("Logger thread terminating"));
+
+  PsTerminateSystemThread (STATUS_SUCCESS);
 }
 
 void
-Logger::WriteRaw (void * data, size_t dataSize)
+Logger::LogUrb (const URB * urb)
+{
+  UrbLogEntry * logEntry = static_cast <UrbLogEntry *> (
+    ExAllocatePool (NonPagedPool, sizeof (UrbLogEntry)));
+
+  if (logEntry == NULL)
+  {
+    KdPrint (("ExAllocatePool failed"));
+    return;
+  }
+
+  LARGE_INTEGER one;
+  one.QuadPart = 1;
+  logEntry->id = ExInterlockedAddLargeInteger (&m_index, one, &m_indexLock);
+
+  KeQuerySystemTime (&logEntry->timestamp);
+
+  logEntry->urb = *urb;
+
+  ExInterlockedPushEntrySList (&m_items, &logEntry->entry, &m_itemsLock);
+}
+
+void
+Logger::WriteUrbEntry (const UrbLogEntry * entry)
+{
+  // Name
+  Write ("Event");
+
+  // Fields
+  Write (6);
+  
+  Write ("id");
+  Write ("%lld", entry->id.QuadPart);
+
+  Write ("type");
+  Write ("IOCTL_INTERNAL_USB_SUBMIT_URB");
+
+  Write ("timestamp");
+  Write ("%lld", entry->timestamp.QuadPart);
+
+  Write ("processName");
+  Write ("ntoskrnl.dll");
+
+  Write ("processId");
+  Write ("0");
+
+  Write ("threadId");
+  Write ("0");
+
+  // Content
+  Write (1); // is raw
+  Write (sizeof (URB));
+  WriteRaw (&entry->urb, sizeof (URB));
+
+  // Children
+  Write (static_cast <ULONG> (0));
+}
+
+void
+Logger::WriteRaw (const void * data, size_t dataSize)
 {
   if (m_fileHandle == NULL)
     return;
 
   NTSTATUS status;
   IO_STATUS_BLOCK io_status;
-  status = ZwWriteFile (m_fileHandle, NULL, NULL, NULL, &io_status, data,
-    static_cast <ULONG> (dataSize), 0, NULL);
-
-  KdPrint (("ZwWriteFile returned 0x%08x", status));
+  status = ZwWriteFile (m_fileHandle, NULL, NULL, NULL, &io_status,
+    const_cast <void *> (data), static_cast <ULONG> (dataSize), 0, NULL);
+  if (!NT_SUCCESS (status))
+    KdPrint (("ZwWriteFile failed: 0x%08x", status));
 }
 
 void
-Logger::WriteLine (const WCHAR * format, ...)
+Logger::Write (ULONG dw)
 {
+  WriteRaw (&dw, sizeof (dw));
+}
+
+void
+Logger::Write (const char * format, ...)
+{
+  NTSTATUS status;
+
   va_list argList;
   va_start (argList, format);
 
-  WCHAR buf[2048];
-  if (!NT_SUCCESS (RtlStringCbVPrintfW (buf, sizeof (buf), format, argList)))
-    return;
+  char buf[1024];
+  status = RtlStringCbVPrintfA (buf, sizeof (buf), format, argList);
+  if (!NT_SUCCESS (status)) return;
 
-  size_t rawLength;
-  if (!NT_SUCCESS (RtlStringCbLengthW (buf, sizeof (buf), &rawLength)))
-    return;
+  size_t length;
+  status = RtlStringCbLengthA (buf, sizeof (buf), &length);
+  if (!NT_SUCCESS (status)) return;
 
-  size_t crlfAndTermSize = (2 + 1) * sizeof (WCHAR);
-  size_t crlfOffset = min (rawLength, sizeof (buf) - crlfAndTermSize);
-  size_t crlfPos = crlfOffset / sizeof (WCHAR);
-  RtlStringCbCopyW (buf + crlfPos, crlfAndTermSize, L"\r\n");
-
-  WriteRaw (buf, crlfOffset + crlfAndTermSize - sizeof (WCHAR));
+  Write (static_cast <ULONG> (length));
+  WriteRaw (buf, length);
 }
-
-#if 0
-static NTSTATUS
-logfile_write_line (const char * format, ...)
-{
-  va_list va;
-
-
-}
-#endif
