61e959aa0c246373623a6699cce64a137ddde184
oleavr <oleavr@60f4c359-591d-0410-bfb7-312270ef6092>
2008-01-07 17:35:38 +0000

    Split out process and USB devices ListView housekeeping into two classes with a common ancestor doing all the boring work.

--- oSpy/Capture/ChooseForm.cs
+++ oSpy/Capture/ChooseForm.cs
@@ -24,9 +24,9 @@ using System.Text;
 using System.Windows.Forms;
 using System.Diagnostics;
 using System.IO;
-using System.Threading;
 using System.Runtime.InteropServices;
 using System.Management;
+using System.Threading;
 
 namespace oSpy.Capture
 {
@@ -34,15 +34,8 @@ namespace oSpy.Capture
     {
         private int checkCount = 0;
 
-        private delegate void AsyncMethodCaller ();
-        private List<AsyncMethodCaller> pendingOperations = new List<AsyncMethodCaller> ();
-
-        private Process[] processList = null;
-        private Dictionary<Process, Icon> processIcons = new Dictionary<Process, Icon> ();
-        private int processViewImageIndex = 0;
-
-        private DeviceList usbDevList = null;
-        private int usbViewImageIndex = 0;
+        private ProcessViewUpdater processViewUpdater;
+        private UsbViewUpdater usbViewUpdater;
 
         private ManagementEventWatcher processStartWatcher = null;
         private ManagementEventWatcher processStopWatcher = null;
@@ -53,6 +46,9 @@ namespace oSpy.Capture
 
             processView.ListViewItemSorter = new ProcessViewItemComparer ();
             usbDevView.ListViewItemSorter = new DeviceViewItemComparer ();
+
+            processViewUpdater = new ProcessViewUpdater (processView);
+            usbViewUpdater = new UsbViewUpdater (usbDevView);
 
             WqlEventQuery startQuery = new WqlEventQuery ();
             startQuery.EventClassName = "Win32_ProcessStartTrace";
@@ -73,14 +69,14 @@ namespace oSpy.Capture
 
         private void ProcessEventArrived (object o, EventArrivedEventArgs e)
         {
-            WaitForPendingOperations ();
-            BeginUpdatingProcessList ();
+            processViewUpdater.Update ();
         }
 
         private void InjectForm_Shown (object sender, EventArgs e)
         {
-            BeginUpdatingProcessList ();
-            BeginUpdatingUsbDeviceList ();
+            processViewUpdater.Update ();
+            usbViewUpdater.Update ();
+
             UpdateButtons ();
         }
 
@@ -89,15 +85,13 @@ namespace oSpy.Capture
             processStartWatcher.Stop ();
             processStopWatcher.Stop ();
 
-            WaitForPendingOperations ();
-        }
+            processViewUpdater.BeginClose ();
+            usbViewUpdater.BeginClose ();
 
-        private void WaitForPendingOperations ()
-        {
-            while (pendingOperations.Count > 0)
+            while (!processViewUpdater.IsClosed || !usbViewUpdater.IsClosed)
             {
-                Thread.Sleep (10);
                 Application.DoEvents ();
+                Thread.Sleep (10);
             }
         }
 
@@ -106,220 +100,7 @@ namespace oSpy.Capture
             base.WndProc (ref m);
 
             if (m.Msg == WinApi.WM_DEVICECHANGE)
-            {
-                WaitForPendingOperations ();
-                BeginUpdatingUsbDeviceList ();
-            }
-        }
-
-        private Icon GetProcessIcon (Process proc)
-        {
-            IntPtr hwnd = proc.MainWindowHandle;
-
-            IntPtr iconHandle = WinApi.SendMessage (hwnd, WinApi.WM_GETICON, WinApi.ICON_SMALL2, 0);
-            if (iconHandle == IntPtr.Zero)
-                iconHandle = WinApi.SendMessage (hwnd, WinApi.WM_GETICON, WinApi.ICON_SMALL, 0);
-            if (iconHandle == IntPtr.Zero)
-                iconHandle = WinApi.SendMessage (hwnd, WinApi.WM_GETICON, WinApi.ICON_BIG, 0);
-            if (iconHandle == IntPtr.Zero)
-                iconHandle = WinApi.GetClassLongPtr (hwnd, WinApi.GCL_HICON);
-            if (iconHandle == IntPtr.Zero)
-                iconHandle = WinApi.GetClassLongPtr (hwnd, WinApi.GCL_HICONSM);
-
-            Icon icon = null;
-
-            if (iconHandle != IntPtr.Zero)
-            {
-                try { icon = Icon.FromHandle (iconHandle); }
-                catch (Exception) {}
-            }
-
-            if (icon == null)
-            {
-                try { icon = Icon.ExtractAssociatedIcon (proc.MainModule.FileName); }
-                catch (Exception) {}
-            }
-
-            return icon;
-        }
-
-        private void BeginUpdatingProcessList ()
-        {
-            AsyncMethodCaller caller = new AsyncMethodCaller (CreateProcessList);
-            caller.BeginInvoke (new AsyncCallback (ProcessListCreated), caller);
-            pendingOperations.Add (caller);
-        }
-
-        private void CreateProcessList ()
-        {
-            int ourSessionId = Process.GetCurrentProcess().SessionId;
-
-            List<Process> result = new List<Process> ();
-            foreach (Process process in Process.GetProcesses ())
-            {
-                if (process.SessionId == ourSessionId)
-                {
-                    result.Add (process);
-                    processIcons[process] = GetProcessIcon (process);
-                }
-            }
-
-            processList = result.ToArray ();
-        }
-
-        private void ProcessListCreated (IAsyncResult ar)
-        {
-            AsyncMethodCaller caller = (AsyncMethodCaller)ar.AsyncState;
-            caller.EndInvoke (ar);
-
-            if (usbDevView.InvokeRequired)
-                Invoke (new AsyncMethodCaller (ApplyProcessList));
-            else
-                ApplyProcessList ();
-
-            pendingOperations.Remove (caller);
-        }
-
-        private void ApplyProcessList ()
-        {
-            Dictionary<int, ListViewItem> oldItems = new Dictionary<int, ListViewItem> ();
-            foreach (ListViewItem item in processView.Items)
-                oldItems[(item.Tag as Process).Id] = item;
-
-            foreach (Process process in processList)
-            {
-                if (oldItems.ContainsKey (process.Id))
-                {
-                    ListViewItem item = oldItems[process.Id];
-                    UpdateListViewItemWithProcess (item, process);
-                    oldItems.Remove (process.Id);
-                }
-                else
-                {
-                    ListViewItem item = new ListViewItem ();
-                    UpdateListViewItemWithProcess (item, process);
-                    processView.Items.Add (item);
-                }
-            }
-
-            foreach (ListViewItem item in oldItems.Values)
-                processView.Items.Remove (item);
-
-            processView.Sort ();
-        }
-
-        private void UpdateListViewItemWithProcess (ListViewItem item, Process process)
-        {
-            item.Name = String.Format ("{0} ({1})", process.ProcessName, process.Id);
-            item.Text = item.Name;
-            item.Tag = process;
-
-            Icon icon = processIcons[process];
-            if (item.ImageIndex >= 0)
-            {
-                if (icon != null)
-                    processImagesSmall.Images[item.ImageIndex] = icon.ToBitmap ();
-            }
-            else
-            {
-                if (icon != null)
-                {
-                    processImagesSmall.Images.Add (icon);
-
-                    item.ImageIndex = processViewImageIndex++;
-                }
-            }
-        }
-
-        private void BeginUpdatingUsbDeviceList ()
-        {
-            AsyncMethodCaller caller = new AsyncMethodCaller (CreateUsbDeviceList);
-            caller.BeginInvoke (new AsyncCallback (UsbDeviceListCreated), caller);
-            pendingOperations.Add (caller);
-        }
-
-        private void CreateUsbDeviceList ()
-        {
-            if (usbDevList != null)
-                usbDevList.Dispose ();
-            usbDevList = new DeviceList (DeviceEnumerator.USB);
-        }
-
-        private void UsbDeviceListCreated (IAsyncResult ar)
-        {
-            AsyncMethodCaller caller = (AsyncMethodCaller)ar.AsyncState;
-            caller.EndInvoke (ar);
-
-            if (usbDevView.InvokeRequired)
-                Invoke (new AsyncMethodCaller (ApplyUsbDeviceList));
-            else
-                ApplyUsbDeviceList ();
-
-            pendingOperations.Remove (caller);
-        }
-
-        private void ApplyUsbDeviceList ()
-        {
-            if (usbDevView.Items.Count == 0)
-                usbImagesSmall.ImageSize = new Size (16, 16);
-
-            Dictionary<string, ListViewItem> oldItems = new Dictionary<string, ListViewItem> ();
-            foreach (ListViewItem item in usbDevView.Items)
-            {
-                Device device = item.Tag as Device;
-                oldItems[device.HardwareId] = item;
-            }
-
-            foreach (Device device in usbDevList.Devices)
-            {
-                if (oldItems.ContainsKey (device.HardwareId))
-                {
-                    ListViewItem item = oldItems[device.HardwareId];
-                    UpdateListViewItemWithDevice (item, device);
-                    oldItems.Remove (device.HardwareId);
-                }
-                else
-                {
-                    ListViewItem item = new ListViewItem (device.Name);
-                    UpdateListViewItemWithDevice (item, device);
-                    usbDevView.Items.Add (item);
-                }
-            }
-
-            foreach (ListViewItem item in oldItems.Values)
-                usbDevView.Items.Remove (item);
-
-            usbDevView.Sort ();
-        }
-
-        private void UpdateListViewItemWithDevice (ListViewItem item, Device device)
-        {
-            item.Name = device.Name;
-            item.Tag = device;
-
-            item.ForeColor = (device.Present) ? Color.Green : Color.Black;
-
-            if (device.HasLowerFilter (Constants.UsbAgentName))
-                item.Checked = true;
-
-            if (item.ImageIndex >= 0)
-            {
-                usbImagesSmall.Images[item.ImageIndex] = device.SmallIcon.ToBitmap ();
-                usbImagesLarge.Images[item.ImageIndex] = device.LargeIcon.ToBitmap ();
-            }
-            else
-            {
-                Icon smallIcon = device.SmallIcon;
-                Icon largeIcon = device.LargeIcon;
-
-                if (smallIcon != null && largeIcon != null)
-                {
-                    usbImagesSmall.Images.Add (smallIcon);
-                    usbImagesLarge.Images.Add (largeIcon);
-
-                    item.ImageIndex = usbViewImageIndex++;
-                }
-            }
+                usbViewUpdater.Update ();
         }
 
         private void UpdateButtons ()
@@ -397,6 +178,278 @@ namespace oSpy.Capture
                 return b.Present.CompareTo (a.Present);
             else
                 return a.Name.CompareTo (b.Name);
+        }
+    }
+
+    public abstract class ListViewUpdater
+    {
+        protected ListView view;
+        protected ImageList imageList;
+        protected int imageListIndex = 0;
+        private Thread workerThread;
+        private EventWaitHandle stopEvent;
+        private EventWaitHandle workEvent;
+        private bool hasUpdated = false;
+
+        private delegate object[] CreateItemsDelegate ();
+        private delegate void ApplyItemsDelegate (object[] items);
+
+        public ListViewUpdater (ListView view)
+        {
+            this.view = view;
+            imageList = new ImageList ();
+            view.SmallImageList = imageList;
+
+            workerThread = new Thread (WorkerThreadFunc);
+            stopEvent = new EventWaitHandle (false, EventResetMode.ManualReset);
+            workEvent = new EventWaitHandle (false, EventResetMode.ManualReset);
+            workerThread.Start ();
+        }
+
+        public void Close ()
+        {
+            BeginClose ();
+
+            while (!workerThread.Join (50))
+            {
+                Application.DoEvents ();
+            }
+        }
+
+        public void BeginClose ()
+        {
+            stopEvent.Set ();
+        }
+
+        public bool IsClosed
+        {
+            get { return !workerThread.IsAlive; }
+        }
+
+        public void Update ()
+        {
+            workEvent.Set ();
+        }
+
+        private void WorkerThreadFunc ()
+        {
+            WaitHandle[] handles = { this.stopEvent, this.workEvent };
+
+            int index = 0;
+
+            do
+            {
+                index = EventWaitHandle.WaitAny (handles, Timeout.Infinite, false);
+                if (index == 1)
+                {
+                    workEvent.Reset ();
+
+                    if (hasUpdated)
+                    {
+                        index = EventWaitHandle.WaitAny (handles, 500, false);
+                        if (index == WaitHandle.WaitTimeout)
+                        {
+                            DoUpdate ();
+                        }
+                    }
+                    else
+                    {
+                        hasUpdated = true;
+                        DoUpdate ();
+                    }
+                }
+            }
+            while (index != 0);
+        }
+
+        private void DoUpdate ()
+        {
+            object[] items = CreateItems ();
+
+            if (stopEvent.WaitOne (0, false))
+                return;
+
+            view.Invoke (new ApplyItemsDelegate (ApplyItems), new object[] { items, });
+        }
+
+        protected abstract object[] CreateItems ();
+        protected abstract object GetKeyFromItem (object item);
+        protected abstract void UpdateListViewItem (ListViewItem viewItem, object item);
+
+        private void ApplyItems (object[] items)
+        {
+            System.Collections.Hashtable oldItems = new System.Collections.Hashtable (items.Length);
+            foreach (ListViewItem viewItem in view.Items)
+            {
+                object key = GetKeyFromItem (viewItem.Tag);
+                oldItems[key] = viewItem;
+            }
+
+            foreach (object item in items)
+            {
+                object key = GetKeyFromItem (item);
+
+                if (oldItems.ContainsKey (key))
+                {
+                    ListViewItem viewItem = oldItems[key] as ListViewItem;
+                    UpdateListViewItem (viewItem, item);
+                    oldItems.Remove (key);
+                }
+                else
+                {
+                    ListViewItem viewItem = new ListViewItem ();
+                    UpdateListViewItem (viewItem, item);
+                    view.Items.Add (viewItem);
+                }
+            }
+
+            foreach (ListViewItem item in oldItems.Values)
+                view.Items.Remove (item);
+
+            view.Sort ();
+        }
+    }
+
+    public class ProcessViewUpdater : ListViewUpdater
+    {
+        private Dictionary<Process, Icon> processIcons = new Dictionary<Process, Icon> ();
+
+        public ProcessViewUpdater (ListView view)
+            : base (view)
+        {
+        }
+
+        protected override object[] CreateItems ()
+        {
+            int ourSessionId = Process.GetCurrentProcess ().SessionId;
+
+            List<Process> result = new List<Process> ();
+            foreach (Process process in Process.GetProcesses ())
+            {
+                if (process.SessionId == ourSessionId)
+                {
+                    result.Add (process);
+                    processIcons[process] = GetProcessIcon (process);
+                }
+            }
+
+            return result.ToArray ();
+        }
+
+        private Icon GetProcessIcon (Process proc)
+        {
+            IntPtr hwnd = proc.MainWindowHandle;
+
+            IntPtr iconHandle = WinApi.SendMessage (hwnd, WinApi.WM_GETICON, WinApi.ICON_SMALL2, 0);
+            if (iconHandle == IntPtr.Zero)
+                iconHandle = WinApi.SendMessage (hwnd, WinApi.WM_GETICON, WinApi.ICON_SMALL, 0);
+            if (iconHandle == IntPtr.Zero)
+                iconHandle = WinApi.SendMessage (hwnd, WinApi.WM_GETICON, WinApi.ICON_BIG, 0);
+            if (iconHandle == IntPtr.Zero)
+                iconHandle = WinApi.GetClassLongPtr (hwnd, WinApi.GCL_HICON);
+            if (iconHandle == IntPtr.Zero)
+                iconHandle = WinApi.GetClassLongPtr (hwnd, WinApi.GCL_HICONSM);
+
+            Icon icon = null;
+
+            if (iconHandle != IntPtr.Zero)
+            {
+                try { icon = Icon.FromHandle (iconHandle); }
+                catch (Exception) { }
+            }
+
+            if (icon == null)
+            {
+                try { icon = Icon.ExtractAssociatedIcon (proc.MainModule.FileName); }
+                catch (Exception) { }
+            }
+
+            return icon;
+        }
+
+        protected override object GetKeyFromItem (object item)
+        {
+            return (item as Process).Id;
+        }
+
+        protected override void UpdateListViewItem (ListViewItem viewItem, object item)
+        {
+            Process process = item as Process;
+
+            viewItem.Name = String.Format ("{0} ({1})", process.ProcessName, process.Id);
+            viewItem.Text = viewItem.Name;
+            viewItem.Tag = process;
+
+            Icon icon = processIcons[process];
+            if (viewItem.ImageIndex >= 0)
+            {
+                if (icon != null)
+                    imageList.Images[viewItem.ImageIndex] = icon.ToBitmap ();
+            }
+            else
+            {
+                if (icon != null)
+                {
+                    imageList.Images.Add (icon);
+
+                    viewItem.ImageIndex = imageListIndex++;
+                }
+            }
+        }
+    }
+
+    public class UsbViewUpdater : ListViewUpdater
+    {
+        private DeviceList devList = null;
+
+        public UsbViewUpdater (ListView view)
+            : base (view)
+        {
+        }
+
+        protected override object[] CreateItems ()
+        {
+            if (devList != null)
+                devList.Dispose ();
+
+            devList = new DeviceList (DeviceEnumerator.USB);
+            return devList.Devices.ToArray ();
+        }
+
+        protected override object GetKeyFromItem (object item)
+        {
+            return (item as Device).HardwareId;
+        }
+
+        protected override void UpdateListViewItem (ListViewItem viewItem, object item)
+        {
+            Device device = item as Device;
+
+            viewItem.Name = device.Name;
+            viewItem.Text = viewItem.Name;
+            viewItem.Tag = device;
+
+            viewItem.ForeColor = (device.Present) ? Color.Green : Color.Black;
+
+            if (device.HasLowerFilter (Constants.UsbAgentName))
+                viewItem.Checked = true;
+
+            if (viewItem.ImageIndex >= 0)
+            {
+                imageList.Images[viewItem.ImageIndex] = device.SmallIcon.ToBitmap ();
+            }
+            else
+            {
+                Icon smallIcon = device.SmallIcon;
+                Icon largeIcon = device.LargeIcon;
+
+                if (smallIcon != null && largeIcon != null)
+                {
+                    imageList.Images.Add (smallIcon);
+
+                    viewItem.ImageIndex = imageListIndex++;
+                }
+            }
         }
     }
 }
