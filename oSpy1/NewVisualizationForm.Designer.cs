d15e27fd2997ac5057c9e4e0557c8f434d279ef0
oleavr <oleavr@60f4c359-591d-0410-bfb7-312270ef6092>
2009-04-04 12:45:22 +0000

    [visualization] Enable check on click for improved user experience.

--- oSpy1/NewVisualizationForm.Designer.cs
+++ oSpy1/NewVisualizationForm.Designer.cs
@@ -28,72 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.visualizersBox = new System.Windows.Forms.CheckedListBox();
-            this.okBtn = new System.Windows.Forms.Button();
-            this.cancelBtn = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.SuspendLayout();
+            this.visualizersBox = new System.Windows.Forms.CheckedListBox ();
+            this.okBtn = new System.Windows.Forms.Button ();
+            this.cancelBtn = new System.Windows.Forms.Button ();
+            this.label1 = new System.Windows.Forms.Label ();
+            this.SuspendLayout ();
             // 
             // visualizersBox
             // 
             this.visualizersBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
                         | System.Windows.Forms.AnchorStyles.Left)
                         | System.Windows.Forms.AnchorStyles.Right)));
+            this.visualizersBox.CheckOnClick = true;
             this.visualizersBox.FormattingEnabled = true;
-            this.visualizersBox.Location = new System.Drawing.Point(12, 27);
+            this.visualizersBox.Location = new System.Drawing.Point (12, 27);
             this.visualizersBox.Name = "visualizersBox";
-            this.visualizersBox.Size = new System.Drawing.Size(268, 199);
+            this.visualizersBox.Size = new System.Drawing.Size (268, 199);
             this.visualizersBox.TabIndex = 0;
-            this.visualizersBox.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.visualizersBox_ItemCheck);
+            this.visualizersBox.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler (this.visualizersBox_ItemCheck);
             // 
             // okBtn
             // 
             this.okBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.okBtn.DialogResult = System.Windows.Forms.DialogResult.Cancel;
             this.okBtn.Enabled = false;
-            this.okBtn.Location = new System.Drawing.Point(123, 238);
+            this.okBtn.Location = new System.Drawing.Point (123, 238);
             this.okBtn.Name = "okBtn";
-            this.okBtn.Size = new System.Drawing.Size(75, 23);
+            this.okBtn.Size = new System.Drawing.Size (75, 23);
             this.okBtn.TabIndex = 1;
             this.okBtn.Text = "&OK";
             this.okBtn.UseVisualStyleBackColor = true;
-            this.okBtn.Click += new System.EventHandler(this.okBtn_Click);
+            this.okBtn.Click += new System.EventHandler (this.okBtn_Click);
             // 
             // cancelBtn
             // 
             this.cancelBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.cancelBtn.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.cancelBtn.Location = new System.Drawing.Point(205, 238);
+            this.cancelBtn.Location = new System.Drawing.Point (205, 238);
             this.cancelBtn.Name = "cancelBtn";
-            this.cancelBtn.Size = new System.Drawing.Size(75, 23);
+            this.cancelBtn.Size = new System.Drawing.Size (75, 23);
             this.cancelBtn.TabIndex = 2;
             this.cancelBtn.Text = "Cancel";
             this.cancelBtn.UseVisualStyleBackColor = true;
-            this.cancelBtn.Click += new System.EventHandler(this.cancelBtn_Click);
+            this.cancelBtn.Click += new System.EventHandler (this.cancelBtn_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Location = new System.Drawing.Point (12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(129, 13);
+            this.label1.Size = new System.Drawing.Size (129, 13);
             this.label1.TabIndex = 3;
             this.label1.Text = "Choose visualizers to use:";
             // 
             // NewVisualizationForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF (6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(292, 273);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.cancelBtn);
-            this.Controls.Add(this.okBtn);
-            this.Controls.Add(this.visualizersBox);
+            this.ClientSize = new System.Drawing.Size (292, 273);
+            this.Controls.Add (this.label1);
+            this.Controls.Add (this.cancelBtn);
+            this.Controls.Add (this.okBtn);
+            this.Controls.Add (this.visualizersBox);
             this.Name = "NewVisualizationForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "New visualization";
-            this.ResumeLayout(false);
-            this.PerformLayout();
+            this.ResumeLayout (false);
+            this.PerformLayout ();
 
         }
 
