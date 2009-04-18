61b0153856cf15a15a7f20eb02c1ce3cf9becd4a
oleavr <oleavr@60f4c359-591d-0410-bfb7-312270ef6092>
2009-04-18 11:20:39 +0000

    Add test for DumpLoader.LoadAsync() and adjust implementation accordingly.

--- trunk/oSpy.SharpDumpLib/Tests/DumpLoaderTest.cs
+++ trunk/oSpy.SharpDumpLib/Tests/DumpLoaderTest.cs
@@ -15,7 +15,10 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 
+using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
+using System.Threading;
 using NUnit.Framework;
 using NUnit.Framework.SyntaxHelpers;
 
@@ -38,10 +41,45 @@ namespace oSpy.SharpDumpLib.Tests
             LoadAndVerifyEvents(stream);
         }
 
-        private static void LoadAndVerifyEvents(Stream stream)
+        [Test ()]
+        public void LoadAsync()
+        {
+            Stream stream = TestOsdStream.GenerateUncompressedFrom(TestEventXml.E001_Error, TestEventXml.E083_CreateSocket, TestEventXml.E140_CloseSocket);
+            DumpLoader loader = new DumpLoader();
+
+            AutoResetEvent completed = new AutoResetEvent(false);
+            Dump dump = null;
+            List<ProgressChangedEventArgs> progressEvents = new List<ProgressChangedEventArgs>();
+            loader.LoadProgressChanged += delegate(object sender, ProgressChangedEventArgs e)
+                                          {
+                                              lock (progressEvents)
+                                                  progressEvents.Add(e);
+                                          };
+            loader.LoadCompleted += delegate(object sender, LoadCompletedEventArgs e)
+                                    {
+                                        dump = e.Dump;
+                                        completed.Set();
+                                    };
+            loader.LoadAsync(stream, this);
+
+            completed.WaitOne();
+
+            VerifyEvents(dump);
+            Assert.That(progressEvents.Count, Is.EqualTo(3));
+            Assert.That(progressEvents[0].ProgressPercentage, Is.EqualTo(33));
+            Assert.That(progressEvents[1].ProgressPercentage, Is.EqualTo(66));
+            Assert.That(progressEvents[2].ProgressPercentage, Is.EqualTo(100));
+        }
+
+        private void LoadAndVerifyEvents(Stream stream)
         {
             DumpLoader loader = new DumpLoader();
             Dump dump = loader.Load(stream);
+            VerifyEvents(dump);
+        }
+
+        private void VerifyEvents(Dump dump)
+        {
             Assert.That(dump.Events.Count, Is.EqualTo(3));
             Assert.That(dump.Events.Keys, Is.EquivalentTo(new uint[] { 1, 83, 140 }));
             Assert.That(dump.Events[1].Id, Is.EqualTo(1));
