9146610cc1328002db81eaefa43e0814cbcb7805
oleavr <oleavr@60f4c359-591d-0410-bfb7-312270ef6092>
2009-04-19 22:02:37 +0000

    Timeline layout algorithm work in progress.

--- trunk/oSpyStudio/Widgets/TimelineLayoutManager.cs
+++ trunk/oSpyStudio/Widgets/TimelineLayoutManager.cs
@@ -26,7 +26,9 @@ namespace oSpyStudio.Widgets
         protected readonly uint m_ymargin = 10;
         protected readonly uint m_xpadding = 3;
         protected readonly uint m_ypadding = 6;
+
         protected List<ITimelineNode> m_nodes = new List<ITimelineNode>();
+        protected uint m_rowCount;
 
         public uint XMargin
         {
@@ -67,7 +69,15 @@ namespace oSpyStudio.Widgets
                 return m_nodes;
             }
         }
-        
+
+        public uint RowCount
+        {
+            get
+            {
+                return m_rowCount;
+            }
+        }
+
         public void Add(ITimelineNode node)
         {
             m_nodes.Add(node);
@@ -78,14 +88,114 @@ namespace oSpyStudio.Widgets
         {
             m_nodes.Sort(new TimelineNodeTimestampComparer());
 
+            NodeList remainingNodes = new NodeList(m_nodes);
+            RangeList allocatedRanges = new RangeList();
+            RowList rows = new RowList();
+
+            while (remainingNodes.Count > 0)
+            {
+                NodeList relatedNodes = remainingNodes.PopFirstRangeOfRelatedNodes();
+                uint startTime = relatedNodes[0].Timestamp;
+                uint endTime = relatedNodes[relatedNodes.Count - 1].Timestamp; // FIXME: need Duration
+                Range range = new Range(startTime, endTime, relatedNodes);
+
+                int rowIndex = allocatedRanges.GetNumberOfIntersectingRanges(range);
+                allocatedRanges.Add(range);
+
+                if (rowIndex >= rows.Count)
+                    rows.Add(new Row());
+                Row row = rows[rowIndex];
+                row.AddRange(range);
+            }
+
             uint x = m_xmargin;
             uint y = m_ymargin;
-            foreach (ITimelineNode node in m_nodes)
+            foreach (Row row in rows)
             {
-                node.Position = new Point(x, y);
-
-                x += node.Allocation.Width + m_xpadding;
+                
             }
+
+            m_rowCount = (uint) rows.Count;
+        }
+
+        internal class NodeList : List<ITimelineNode>
+        {
+            public NodeList()
+                : base()
+            {
+            }
+
+            public NodeList(IEnumerable<ITimelineNode> collection)
+                : base(collection)
+            {
+            }
+
+            public NodeList PopFirstRangeOfRelatedNodes()
+            {
+                NodeList relatedNodes = new NodeList();
+
+                ITimelineNode firstNode = this[0];
+                relatedNodes.Add(firstNode);
+                RemoveAt(0);
+
+                foreach (ITimelineNode node in this)
+                {
+                    if (node.Context == firstNode.Context)
+                        relatedNodes.Add(node);
+                }
+
+                for (int i = 1; i < relatedNodes.Count; i++)
+                    Remove(relatedNodes[i]);
+
+                return relatedNodes;
+            }
+        }
+
+        internal class Range
+        {
+            public readonly uint Start;
+            public readonly uint End;
+            public readonly NodeList Nodes;
+
+            public Range(uint start, uint end, NodeList nodes)
+            {
+                Start = start;
+                End = end;
+                Nodes = nodes;
+            }
+        }
+
+        internal class RangeList : List<Range>
+        {
+            public int GetNumberOfIntersectingRanges(Range range)
+            {
+                int numIntersections = 0;
+
+                foreach (Range curRange in this)
+                {
+                    if ((curRange.Start >= range.Start && curRange.Start <= range.End) ||
+                        (curRange.End >= range.Start && curRange.End <= range.End))
+                    {
+                        numIntersections++;
+                    }
+                }
+
+                return numIntersections;
+            }
+        }
+
+        internal class Row
+        {
+            public RangeList Ranges = new RangeList();
+
+            public void AddRange(Range range)
+            {
+                Ranges.Add(range);
+            }
+        }
+
+        internal class RowList : List<Row>
+        {
         }
     }
 
