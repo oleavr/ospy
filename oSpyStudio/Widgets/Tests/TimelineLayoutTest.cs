14bce4f6edc7a842504527b9c2d2e6c6adc7d1cb
oleavr <oleavr@60f4c359-591d-0410-bfb7-312270ef6092>
2009-04-19 17:02:46 +0000

    Use padding between nodes, and add a failing test for two overlapping contexts.

--- oSpyStudio/Widgets/Tests/TimelineLayoutTest.cs
+++ oSpyStudio/Widgets/Tests/TimelineLayoutTest.cs
@@ -25,7 +25,7 @@ namespace oSpyStudio.Widgets.Tests
     public class TimelineLayoutTest
     {
         [Test()]
-        public void OneNode()
+        public void OneContextOneNode()
         {
             string context = "Context1";
             ITimelineNode node = new TestNode(10, context, new Size(100, 50));
@@ -37,10 +37,10 @@ namespace oSpyStudio.Widgets.Tests
         }
 
         [Test()]
-        public void TwoNodesSameContext()
+        public void OneContextTwoNodes()
         {
             string context = "Context1";
-            ITimelineNode nodeA = new TestNode(10,  context, new Size(80, 50));
+            ITimelineNode nodeA = new TestNode(10, context, new Size(80, 50));
             ITimelineNode nodeB = new TestNode(20, context, new Size(100, 40));
             TimelineLayoutManager layout = new TimelineLayoutManager();
             layout.Add(nodeB);
@@ -49,7 +49,33 @@ namespace oSpyStudio.Widgets.Tests
             Assert.That(layout.Nodes[0], Is.SameAs(nodeA));
             Assert.That(layout.Nodes[1], Is.SameAs(nodeB));
             Assert.That(nodeA.Position, Is.EqualTo(new Point(layout.XMargin, layout.YMargin)));
-            Assert.That(nodeB.Position, Is.EqualTo(new Point(layout.XMargin + 80 + layout.XMargin, layout.YMargin)));
+            Assert.That(nodeB.Position, Is.EqualTo(new Point(layout.XMargin + 80 + layout.XPadding, layout.YMargin)));
+        }
+
+        [Test()]
+        public void TwoContextsOverlappingNodes()
+        {
+            string context1 = "Context1";
+            ITimelineNode ctx1nodeA = new TestNode(10, context1, new Size(80, 50));
+            ITimelineNode ctx1nodeB = new TestNode(20, context1, new Size(100, 40));
+            string context2 = "Context2";
+            ITimelineNode ctx2node = new TestNode(15, context2, new Size(50, 50));
+
+            TimelineLayoutManager layout = new TimelineLayoutManager();
+            layout.Add(ctx2node);
+            layout.Add(ctx1nodeB);
+            layout.Add(ctx1nodeA);
+
+            Assert.That(layout.Nodes.Count, Is.EqualTo(3));
+            Assert.That(layout.Nodes[0], Is.SameAs(ctx1nodeA));
+            Assert.That(layout.Nodes[1], Is.SameAs(ctx2node));
+            Assert.That(layout.Nodes[2], Is.SameAs(ctx1nodeB));
+
+            Assert.That(ctx1nodeA.Position, Is.EqualTo(new Point(layout.XMargin, layout.YMargin)));
+            Assert.That(ctx1nodeB.Position, Is.EqualTo(new Point(layout.XMargin + 80 + layout.XPadding, layout.YMargin)));
+
+            Assert.That(ctx2node.Position.X, Is.EqualTo(ctx1nodeA.Position.X + (ctx1nodeA.Allocation.Width + layout.XPadding + ctx1nodeB.Allocation.Width) / 2));
+            Assert.That(ctx2node.Position.Y, Is.EqualTo(layout.YMargin + 50 + layout.YMargin));
         }
     }
 }
