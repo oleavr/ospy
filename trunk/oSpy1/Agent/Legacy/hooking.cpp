1afc75aa11d5e162727c68336f28b777e8067333
oleavr <oleavr@60f4c359-591d-0410-bfb7-312270ef6092>
2009-08-17 23:03:45 +0000

    Rewrite signature parsing code and improve the matching algorithm.
    The latter is now a bit faster as we don't bother scanning non-executable memory.

--- trunk/oSpy1/Agent/Legacy/hooking.cpp
+++ trunk/oSpy1/Agent/Legacy/hooking.cpp
@@ -112,272 +112,132 @@ typedef enum {
     TOKEN_TYPE_IGNORE = 1,
 } SignatureTokenType;
 
-typedef struct {
+class SignatureToken : public BaseObject
+{
+public:
+    SignatureToken(SignatureTokenType type)
+        : type(type), length(0)
+    {
+    }
+
     SignatureTokenType type;
-    int length;
-    unsigned char *data;
-} SignatureToken;
-
-static bool
-find_next_token(const char **input, int *num_ignores, char **error)
-{
-    const char *p = *input;
-    int chars_skipped = 0, qms_skipped = 0;
-    *num_ignores = 0;
-
-    for (; *p != '\0'; p++)
-    {
-        if ((*p >= 48 && *p <= 57) ||
-            (*p >= 65 && *p <= 90) ||
-            (*p >= 97 && *p <= 122))
-        {
-            chars_skipped++;
-
-            if (chars_skipped == 3)
-            {
-                goto DONE;
-            }
-        }
-        else if (*p == '?')
-        {
-            qms_skipped++;
-        }
-    }
-
-    if (*p == '\0')
-        p = NULL;
-
-DONE:
-    *input = p;
-    *num_ignores = qms_skipped / 2;
-    return true;
-}
-
-static SignatureToken *
-signature_token_new (SignatureTokenType type, int buffer_size)
-{
-    SignatureToken *token;
-
-    token = (SignatureToken *) sspy_malloc(sizeof(SignatureToken));
-    token->type = type;
-    token->length = 0;
-
-    if (buffer_size > 0)
-        token->data = (unsigned char *) sspy_malloc(buffer_size);
-    else
-        token->data = NULL;
-
-    return token;
-}
-
-static void
-signature_token_free (SignatureToken *token)
-{
-    if (token->data != NULL)
-    {
-        sspy_free(token->data);
-    }
-
-    sspy_free(token);
-}
-
-static void
-signature_token_list_free (SignatureToken **tokens)
-{
-    int i;
-
-    for (i = 0; i; i++)
-    {
-        SignatureToken *cur_token = tokens[i];
-
-        if (cur_token != NULL)
-        {
-            signature_token_free(cur_token);
-        }
-        else
-        {
-            break;
-        }
-    }
-
-    sspy_free(tokens);
-}
-
-static void
-signature_token_print (SignatureToken *token)
-{
-    fprintf(stderr, "type=%s, length=%d",
-        (token->type == TOKEN_TYPE_LITERAL) ? "TOKEN_TYPE_LITERAL" : "TOKEN_TYPE_IGNORE ",
-        token->length);
-
-    if (token->data != NULL)
-    {
-        int j;
-
-        fprintf(stderr, ", data=[");
-        for (j = 0; j < token->length; j++)
-        {
-            fprintf(stderr, " %02hhx", token->data[j]);
-        }
-
-        fprintf(stderr, "]");
-    }
-
-    fprintf(stderr, "\n");
-}
+    DWORD length;
+    OString data;
+};
 
 static bool
 parse_signature(const FunctionSignature *sig,
-                SignatureToken ***tokens,
-                int *num_tokens,
-                int *raw_size,
-                SignatureToken **longest_token,
-                int *longest_offset,
+                OVector<SignatureToken>::Type &tokens,
+                DWORD *raw_size,
+                DWORD *longest_token_idx,
+                DWORD *longest_token_offset,
                 char **error)
 {
+    int cur_token_idx = -1;
+
     const char *p;
-    int sig_len, max_tokens, token_index, buf_pos, i, offset;
-    SignatureToken **ret_tokens, *cur_token;
-
-    ret_tokens = NULL;
-
-    /* Count the number of '?' characters in order to get
-     * an idea of the maximum number of tokens we're
-     * going to parse, and find the string length while
-     * we're at it. */
-    sig_len = max_tokens = 0;
-    for (p = sig->signature; *p != '\0'; p++)
+    for (p = sig->signature; *p != '\0';)
     {
-        sig_len++;
-
-        if (*p == '?')
-            max_tokens++;
-    }
-
-    if (max_tokens == 0)
-    {
-        max_tokens++;
-    }
-    else
-    {
-        if (max_tokens % 2 != 0)
+        if (p[0] == '?')
         {
-            *error = sspy_strdup("syntax error: unbalanced questionmarks");
-            goto ERR_OUT;
-        }
-
-        max_tokens = (max_tokens / 2) + 1;
-    }
-
-    /* Make it NULL-terminated. */
-    max_tokens++;
-
-    /* Allocate an array of token-pointers. */
-    ret_tokens = (SignatureToken **) sspy_malloc(max_tokens * sizeof(SignatureToken *));
-    memset(ret_tokens, 0, max_tokens * sizeof(SignatureToken *));
-    cur_token = NULL;
-    token_index = 0;
-
-    /* Start parsing. */
-    p = sig->signature;
-    while (true)
-    {
-        unsigned int tmp;
-        int ret, ignores;
-
-        ret = sscanf(p, " %02x", &tmp);
-        if (ret != 1)
-        {
-            break;
-        }
-
-        if (cur_token == NULL)
-        {
-            cur_token = signature_token_new(TOKEN_TYPE_LITERAL, sig_len + 1);
-            buf_pos = 0;
-        }
-
-        cur_token->data[buf_pos] = tmp;
-        cur_token->length++;
-        buf_pos++;
-
-        if (!find_next_token(&p, &ignores, error))
-        {
-            goto ERR_OUT;
-        }
-
-        if (ignores > 0)
-        {
-            ret_tokens[token_index++] = cur_token;
-
-            cur_token = signature_token_new(TOKEN_TYPE_IGNORE, 0);
-            cur_token->length = ignores;
-            ret_tokens[token_index++] = cur_token;
-
-            cur_token = NULL;
-        }
-
-        if (p == NULL)
-        {
-            break;
-        }
-    }
-
-    if (cur_token != NULL)
-    {
-        ret_tokens[token_index++] = cur_token;
-    }
-
-    *tokens = ret_tokens;
-    *num_tokens = token_index;
-
-    /* Find the longest token and its offset. */
-    offset = 0;
-    *longest_token = ret_tokens[0];
-    *longest_offset = 0;
-    for (i = 0; i < *num_tokens; i++)
-    {
-        cur_token = ret_tokens[i];
-
-        if (cur_token->type == TOKEN_TYPE_LITERAL)
-        {
-            if (cur_token->length > (*longest_token)->length)
+            if (p[1] != '?')
             {
-                *longest_token = cur_token;
-                *longest_offset = offset;
+                *error = sspy_strdup("syntax error: unbalanced questionmarks");
+                return false;
+            }
+
+            if (cur_token_idx < 0 || tokens[cur_token_idx].type != TOKEN_TYPE_IGNORE)
+            {
+                SignatureToken tok(TOKEN_TYPE_IGNORE);
+                tokens.push_back(tok);
+                cur_token_idx = tokens.size() - 1;
+            }
+
+            tokens[cur_token_idx].length++;
+
+            p += 2;
+        }
+        else if (isxdigit(p[0]))
+        {
+            if (!isxdigit(p[1]))
+            {
+                *error = sspy_strdup("syntax error: incomplete hex byte");
+                return false;
+            }
+
+            if (cur_token_idx < 0 || tokens[cur_token_idx].type != TOKEN_TYPE_LITERAL)
+            {
+                SignatureToken tok(TOKEN_TYPE_LITERAL);
+                tokens.push_back(tok);
+                cur_token_idx = tokens.size() - 1;
+            }
+
+            tokens[cur_token_idx].length++;
+
+            char str[3] = { tolower(p[0]), tolower(p[1]), '\0' };
+            int parsed_value;
+            sscanf(str, "%02x", &parsed_value);
+            BYTE value = parsed_value & 0xff;
+            tokens[cur_token_idx].data.append(reinterpret_cast<char *>(&value), sizeof(value));
+
+            p += 2;
+        }
+        else if (isspace(*p))
+        {
+            p++;
+        }
+        else
+        {
+            *error = sspy_strdup("syntax error: expected byte specifier");
+            return false;
+        }
+    }
+
+    DWORD index = 0, offset = 0;
+    DWORD longest = 0;
+
+    OVector<SignatureToken>::Type::const_iterator it;
+    for (it = tokens.begin(); it != tokens.end(); ++it)
+    {
+        if (it->type == TOKEN_TYPE_LITERAL)
+        {
+            if (it->length > longest)
+            {
+                *longest_token_idx = index;
+                *longest_token_offset = offset;
+
+                longest = it->length;
             }
         }
 
-        offset += cur_token->length;
+        index++;
+        offset += it->length;
+    }
+
+    if (longest == 0)
+    {
+        *error = sspy_strdup("error: no match bytes specified");
+        return false;
     }
 
     *raw_size = offset;
-    
+
     return true;
-
-ERR_OUT:
-    if (ret_tokens != NULL)
-    {
-        signature_token_list_free(ret_tokens);
-    }
-
-    return false;
 }
 
 static bool
-scan_against_all_tokens(unsigned char *p, SignatureToken **tokens)
+scan_against_all_tokens(unsigned char *p, const OVector<SignatureToken>::Type tokens)
 {
-    SignatureToken **cur_token;
-
-    for (cur_token = tokens; *cur_token != NULL; cur_token++)
+    OVector<SignatureToken>::Type::const_iterator it;
+    for (it = tokens.begin(); it != tokens.end(); ++it)
     {
-        if ((*cur_token)->type == TOKEN_TYPE_LITERAL)
+        if (it->type == TOKEN_TYPE_LITERAL)
         {
-            if (memcmp(p, (*cur_token)->data, (*cur_token)->length) != 0)
+            if (memcmp(p, it->data.data(), it->length) != 0)
                 return false;
         }
 
-        p += (*cur_token)->length;
+        p += it->length;
     }
 
     return true;
@@ -392,63 +252,90 @@ find_signature(const FunctionSignature *sig, LPVOID *address, char **error)
 bool
 find_signature_in_module(const FunctionSignature *sig, const char *module_name, LPVOID *address, char **error)
 {
-    bool result = false;
-    unsigned char *base, *p, *match;
+    BYTE *base;
     DWORD size;
-    SignatureToken **tokens = NULL, *longest_token;
-    int num_tokens, raw_size, longest_offset, num_matches;
-
     if (!get_module_base_and_size(module_name, (LPVOID *) &base, &size, error))
-    {
-        goto DONE;
-    }
+        return false;
 
-    if (!parse_signature(sig, &tokens, &num_tokens, &raw_size, &longest_token,
-                         &longest_offset, error))
-    {
-        goto DONE;
-    }
+    OVector<SignatureToken>::Type tokens;
+    DWORD raw_size;
+    DWORD longest_token_idx, longest_token_offset;
+    if (!parse_signature(sig, tokens, &raw_size, &longest_token_idx, &longest_token_offset, error))
+        return false;
+    SignatureToken *longest_token = &tokens[longest_token_idx];
 
-    match = NULL;
-    num_matches = 0;
-    for (p = base; p < base + size - raw_size; p++)
+    BYTE *p, *match = NULL;
+    DWORD num_matches = 0;
+
+    OVector<MEMORY_BASIC_INFORMATION>::Type regions;
+    DWORD page_bytes_left = 0;
+
+    for (p = base; p < base + size - raw_size;)
     {
-        if (memcmp(p, longest_token->data, longest_token->length) == 0)
+        if (page_bytes_left < raw_size)
         {
-            bool matched = scan_against_all_tokens(p - longest_offset, tokens);
+            MEMORY_BASIC_INFORMATION mbi;
+            SIZE_T result = VirtualQuery(p + page_bytes_left, &mbi, sizeof(mbi));
+            _ASSERT(result == sizeof(mbi));
+
+            if ((mbi.Protect & (PAGE_NOACCESS | PAGE_READONLY | PAGE_READWRITE | PAGE_WRITECOPY)) != 0 ||
+                (mbi.Protect & PAGE_GUARD) != 0)
+            {
+                page_bytes_left = 0;
+                p += mbi.RegionSize;
+                continue;
+            }
+
+            if ((mbi.Protect & PAGE_EXECUTE) != 0)
+            {
+                DWORD old_protect;
+                VirtualProtect(p + page_bytes_left, mbi.RegionSize, PAGE_EXECUTE_READWRITE, &old_protect);
+
+                regions.push_back(mbi);
+            }
+
+            page_bytes_left += mbi.RegionSize;
+        }
+
+        int distance_forward = 1;
+
+        if (memcmp(p, longest_token->data.data(), longest_token->length) == 0)
+        {
+            bool matched = scan_against_all_tokens(p - longest_token_offset, tokens);
             if (matched)
             {
                 num_matches++;
-                match = p - longest_offset;
+                match = p - longest_token_offset;
 
-                /* Skip ahead. */
-                p = p - longest_offset + raw_size;
+                distance_forward = raw_size - longest_token_offset;
             }
         }
+
+        page_bytes_left -= distance_forward;
+        p += distance_forward;
+    }
+
+    OVector<MEMORY_BASIC_INFORMATION>::Type::const_iterator it;
+    for (it = regions.begin(); it != regions.end(); ++it)
+    {
+        DWORD old_protect;
+        VirtualProtect(it->BaseAddress, it->RegionSize, it->Protect, &old_protect);
     }
 
     if (num_matches == 0)
     {
         *error = sspy_strdup("No matches found");
-        goto DONE;
+        return false;
     }
     else if (num_matches > 1)
     {
         *error = sspy_strdup("More than one match found");
-        goto DONE;
+        return false;
     }
 
     *address = match - sig->start_offset;
 
-    result = true;
-
-DONE:
-    if (tokens != NULL)
-    {
-        signature_token_list_free(tokens);
-    }
-
-    return result;
+    return true;
 }
 
 static DWORD
