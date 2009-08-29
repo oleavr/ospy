4a372ae5f340a7e82f43b04a5d777b54188a915d
oleavr <oleavr@60f4c359-591d-0410-bfb7-312270ef6092>
2009-08-29 17:14:59 +0000

    Bump to 1.10.4.

--- oSpy1/Properties/AssemblyInfo.cs
+++ oSpy1/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.10.3.0")]
-[assembly: AssemblyFileVersion("1.10.3.0")]
+[assembly: AssemblyVersion("1.10.4.0")]
+[assembly: AssemblyFileVersion("1.10.4.0")]
